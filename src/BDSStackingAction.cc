afac7fdbc1d2815524398cd71d28e3b14fe9f842
agapov <agapov>
2005-10-11 15:59:20 +0000

    added stacking action to trat secondaries

---
+++ src/BDSStackingAction.cc
@@ -0,0 +1,90 @@
+
+#include "BDSGlobalConstants.hh"
+#include "BDSStackingAction.hh"
+#include "G4SDManager.hh"
+#include "G4RunManager.hh"
+#include "G4Event.hh"
+#include "G4HCofThisEvent.hh"
+#include "G4Track.hh"
+#include "G4TrackStatus.hh"
+#include "G4ParticleDefinition.hh"
+#include "G4ParticleTypes.hh"
+#include "G4ios.hh"
+
+BDSStackingAction::BDSStackingAction()
+{ 
+}
+
+BDSStackingAction::~BDSStackingAction()
+{
+}
+
+G4ClassificationOfNewTrack 
+BDSStackingAction::ClassifyNewTrack(const G4Track * aTrack)
+{
+  G4ClassificationOfNewTrack classification = fWaiting;
+ 
+  if(BDSGlobals->GetStopTracks()) // if tracks killed after interaction
+    {
+      
+      G4cout<<"StackingAction: ClassifyNewtrack "<<aTrack->GetTrackID()<<
+	" "<<aTrack->GetDefinition()->GetParticleName()<<G4endl;
+      
+      // kill secondary electrons
+      
+      if( (aTrack->GetParentID() != 0) && 
+	  (aTrack->GetDefinition() == G4Electron::ElectronDefinition() ) )
+	{
+	  
+	  
+	  classification = fKill;
+	  //classification = fUrgent;
+	}
+      
+      // kill secondary photons
+      
+      if( (aTrack->GetParentID() != 0) && 
+	  (aTrack->GetDefinition() == G4Gamma::GammaDefinition() ) )
+	{
+	  classification = fKill;
+	}
+      
+      // kill secondary positrons
+      
+      if( (aTrack->GetParentID() != 0) && 
+	  (aTrack->GetDefinition() == G4Positron::PositronDefinition() ) )
+	{
+	  classification = fKill;
+	}
+      
+    }
+  
+  return classification;
+}
+
+
+void BDSStackingAction::NewStage()
+{
+  // urgent stack empty, looking into the waiting stack
+ 
+  G4cout<<"StackingAction: New stage"<<G4endl;
+
+
+  // kill event
+  if(1==2)
+    {
+      stackManager->clear();
+      G4cout << "++++++++ event aborted" << G4endl;
+      return;
+    }
+  
+  //  stackManager->ReClassify();
+  return;
+ 
+}
+    
+void BDSStackingAction::PrepareNewEvent()
+{ 
+}
+
+
