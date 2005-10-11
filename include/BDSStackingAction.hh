49ab7e67c5f40a4277633fc595242df7b266f740
agapov <agapov>
2005-10-11 15:59:45 +0000

    added stacking action to treat secondaries

---
+++ include/BDSStackingAction.hh
@@ -0,0 +1,33 @@
+/*
+  Stacking Action - action performed before particles pushed to the stack
+
+*/
+
+
+#ifndef __BDSSTACKING_ACTION__
+#define __BDSSTACKING_ACTION__
+
+#include "globals.hh"
+#include "G4UserStackingAction.hh"
+#include "G4ThreeVector.hh"
+
+class G4Track;
+
+
+class BDSStackingActionMessenger;
+
+class BDSStackingAction : public G4UserStackingAction
+{
+  public:
+    BDSStackingAction();
+    virtual ~BDSStackingAction();
+
+  public:
+    virtual G4ClassificationOfNewTrack ClassifyNewTrack(const G4Track* aTrack);
+    virtual void NewStage();
+    virtual void PrepareNewEvent();
+
+ };
+
+#endif
+
