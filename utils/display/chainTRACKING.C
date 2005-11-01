e033ff4726b1a0979b1ba3611258b89ecc7004e1
dadoun <dadoun>
2005-11-01 08:53:33 +0000

    Read chain with the display

---
+++ utils/display/chainTRACKING.C
@@ -0,0 +1,42 @@
+chainTRACKING(char *gmadFile, char *rootFile)
+{
+	BDSCanvas *b = new BDSCanvas(gmadFile);
+	TChain *PChain;
+	Char_t ChainName[256];
+	Int_t jstar=1;
+	Int_t jend=215;
+	Float_t xmin=1000.,xmax=-1000.;
+	Float_t x,xp,y,yp,z,e;		
+	Char_t temp[256];
+	TNtuple *ntuple = new TNtuple("ntuple","nutple","e:xp:yp:z:x:y");
+	for (Int_t j=jstar; j <= jend ; j++)
+   {
+    	sprintf(ChainName,"samp%d",j);
+    	PChain=new TChain(ChainName,"chain");
+    	PChain->Add(rootFile);
+    	PChain->SetBranchAddress("x", &x);
+    	PChain->SetBranchAddress("xp", &xp);
+   	PChain->SetBranchAddress("y", &y);
+    	PChain->SetBranchAddress("yp", &yp);
+    	PChain->SetBranchAddress("z", &z);
+    	PChain->SetBranchAddress("E", &e);
+    	for (Int_t i=0; i<PChain->GetEntries(); i++)
+   	{
+			PChain->GetEntry(i);
+			ntuple->Fill(e,xp,yp,z,x,y);
+			xmin=TMath::Min(x,xmin);
+			xmax=TMath::Max(x,xmax);
+			
+		}	
+	}
+	sprintf(temp,"x:z>>+temp(100,0,%f,50,%f,%f)",line->xmax_axis,1.5*xmin,1.5*xmax);
+	ntuple->Draw(temp,"e==200","L");
+ 	b->SetHisto(0);
+//	TH2F *histo = new TH2F("histo","using histo",jend,line->xmin_axis,line->xmax_axis,jstar,1.5*xmin,1.5*xmax);
+//	ntuple->Project("histo","x:z","e==200");
+//	histo->Draw("e");
+//
+//	TH2F   *ax = b->GetPlot();
+//	b->SetHisto2(ax);
+ 	return(0);
+}
