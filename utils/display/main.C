76d6749d511e0b8aac1f3ed0f46a4d37ae71eb93
dadoun <dadoun>
2005-11-01 08:54:25 +0000

    New main file

--- utils/display/main.C
+++ utils/display/main.C
@@ -9,26 +9,10 @@ Last modified 30.09.2005
 #include "TH1F.h"
 #include "BDSCanvas.h"
 #include "BDSPad.h"
-
 int main(int argc, char **argv)
 {
-    // Create interactive interface
-    TRint *theApp = new TRint("ROOT example", &argc, argv, NULL, 0);
-	 
-	 // Work Area 
-	 BDSCanvas *b = new BDSCanvas("20mrad.gmad");
-	 
-	 Float_t x,xp,y,yp,z,z0,e;
-	 TFile *f = TFile::Open("20mrad.root");
-	 TH1F *histo;
-	 histo = (TH1F*)f->Get("h1;1"); 
-	 histo->GetXaxis()->SetLimits(BDSPad::xmin_axis,BDSPad::xmax_axis);
-  	 histo->Draw();
-	 b->SetHisto(histo); 
-    b->cd();
-	 // Run interactive interface
+	TRint *theApp = new TRint("ROOT example", &argc, argv, NULL, 0);
 	theApp->Run();
-	
-    return(0);
+	return(0);
 }
 //--------------------------------------------------
