8a1273bc3e3489382a4551979bc9d362670894a0
agapov <agapov>
2005-11-16 14:02:43 +0000

    EM physics without builder

--- include/EMPhysics.hh
+++ include/EMPhysics.hh
@@ -1,11 +1,18 @@
 #ifndef EMPhysics_h
-#define EMPhysics_h 1
+#define EMPhysics_h 
 
 #include "globals.hh"
 #include "G4ios.hh"
 #include "G4VPhysicsConstructor.hh"
-#include "G4EMBuilder.hh"
+//#include "G4EMBuilder.hh"
 
+#include "G4PhotoElectricEffect.hh"
+#include "G4ComptonScattering.hh"
+#include "G4GammaConversion.hh"
+#include "G4MultipleScattering.hh"
+#include "G4eIonisation.hh"
+#include "G4eBremsstrahlung.hh"
+#include "G4eplusAnnihilation.hh"
 
 class EMPhysics : public G4VPhysicsConstructor
 {
@@ -18,10 +25,22 @@ class EMPhysics : public G4VPhysicsConstructor
     virtual void ConstructProcess();
 
   private:
-    G4EMBuilder theEMPhysics;
-};
+  //G4EMBuilder theEMPhysics;
 
-// 2002 by J.P. Wellisch
+  G4PhotoElectricEffect thePhotoEffect;
+  G4ComptonScattering theComptonEffect;
+  G4GammaConversion thePairProduction;
+  
+  G4MultipleScattering theElectronMultipleScattering;
+  G4eIonisation theElectronIonisation;
+  G4eBremsstrahlung theElectronBremsStrahlung;
+  
+  G4MultipleScattering thePositronMultipleScattering;
+  G4eIonisation thePositronIonisation; 
+  G4eBremsstrahlung thePositronBremsStrahlung;  
+  G4eplusAnnihilation theAnnihilation;
+  
+};
 
 #endif
 
