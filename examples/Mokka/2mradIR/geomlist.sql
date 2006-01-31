932da35944d12b81101ece75523d56ffe45e5e9c
carter <carter>
2006-01-31 16:30:03 +0000

    New examples for Mokka/SQL

---
+++ examples/Mokka/2mradIR/geomlist.sql
@@ -0,0 +1,6 @@
+# A list of geometry files to be used
+examples/Mokka/2mradIR/tube-stahl.sql
+examples/Mokka/2mradIR/mask-stahl.sql
+#examples/Mokka/2mradIR/outer-stahl.sql
+examples/Mokka/2mradIR/magnets-stahl.sql
+#examples/Mokka/2mradIR/samplers-stahl.sql
