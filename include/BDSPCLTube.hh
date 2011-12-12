0fbf66abb28b207e3907e5825a587c38370ad848
deacon <deacon>
2011-12-12 10:33:31 +0000

    A geometry for the CLIC post-collision line beam pipe.

---
+++ include/BDSPCLTube.hh
@@ -0,0 +1,27 @@
+#include "G4VSolid.hh"
+
+class BDSPCLTube
+{
+public:
+  BDSPCLTube(G4double aperX, G4double aperYUp, G4double aperYDown, G4double aperDy, G4double thickness, G4double length, G4String name);
+  ~BDSPCLTube();
+  G4VSolid* GetSolid();
+  void Build();
+  void BuildHollow();
+private:
+  G4double itsSizeX;
+  G4double itsSizeYUp;
+  G4double itsSizeYDown;
+  G4double itsSizeDy;
+  G4double itsThickness;
+  G4double itsLength;
+  G4String itsName;
+
+  G4VSolid* upper_solid;
+  G4VSolid* middle_solid;
+  G4VSolid* lower_solid;
+  G4VSolid* temp_solid;
+  G4VSolid* solid;
+
+
+};
