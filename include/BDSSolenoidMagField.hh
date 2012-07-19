27165d0d49c9d6e4bbba391f0d5f8b7131e27eed
jsnuveri <jsnuveri>
2012-07-19 14:52:51 +0000

    fix to commit 668

--- include/BDSSolenoidMagField.hh
+++ include/BDSSolenoidMagField.hh
@@ -13,7 +13,7 @@ public:  // with description
   // Constructor and destructor. No actions.
 
   /// Get Field Value (independent of position, since solenoid field is uniform)
-  virtual void  GetFieldValue( const G4double /*Point[4]*/,
+  virtual void  GetFieldValue( const G4double* /*Point[4]*/,
 			       G4double *Bfield ) const;
   void SetBField(G4double aBField);
 private:
--- src/BDSSolenoidMagField.cc
+++ src/BDSSolenoidMagField.cc
@@ -13,7 +13,7 @@ BDSSolenoidMagField::BDSSolenoidMagField(G4double aBField):itsBField(aBField){
 }
 BDSSolenoidMagField::~BDSSolenoidMagField(){}
 
-void BDSSolenoidMagField::GetFieldValue( const G4double /*Point[4]*/,
+void BDSSolenoidMagField::GetFieldValue( const G4double* /*Point[4]*/,
 				      G4double *Bfield ) const
 {
   G4Navigator* Navigator=
