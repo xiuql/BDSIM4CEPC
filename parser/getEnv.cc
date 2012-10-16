719b81bc97f054fcd8902dac2a82a386b0f7ee73
deacon <deacon>
2012-10-16 15:16:35 +0000

    Function for retrieving environment variables.

---
+++ parser/getEnv.cc
@@ -0,0 +1,16 @@
+#include <string>
+#include <iostream>
+#include "getEnv.h"
+
+const char* getEnv(const char* env)
+{
+  const char* tmp = std::getenv(env);
+  if(tmp==NULL){
+    std::cout << "getEnv - no environment variable \"" << env << "\" is set" << std::endl;
+    return (const char*)"";
+  } else {
+    std::string stmp = (std::string)tmp+ "/";
+    std::cout << "getEnv - environment variable \"" << stmp << "\" is set" << std::endl;
+    return stmp.c_str();
+  }
+}
