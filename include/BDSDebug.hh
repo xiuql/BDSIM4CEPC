2bfcc7a94cbb01aff65d82f7c0fb625a3c28e3ac
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2013-05-18 01:50:33 +0100

    Harmonising printout

--- include/BDSDebug.hh
+++ include/BDSDebug.hh
@@ -4,6 +4,7 @@
 #include <stddef.h>
 #include <string>
 
+
 inline std::string methodName(const std::string& prettyFunction)
 {
   size_t colons = prettyFunction.find("::");
@@ -15,5 +16,15 @@ inline std::string methodName(const std::string& prettyFunction)
 
 #define __METHOD_NAME__ methodName(__PRETTY_FUNCTION__)
 
+#define __BDS_DEBUG_FNC__(message) 
+#ifdef DEBUG 
+G4cout << __FUNCTION__ << ">" << message << G4endl; 
+#endif
+
+#ifdef DEBUG
+#define __BDS_DEBUG_MET__(message) G4cout << __METHOD_NAME__ << ">" << message << G4endl; 
+#else 
+#define __BDS_DEBUG_MET__(message) ;
+#endif
 
 #endif
