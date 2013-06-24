f74df3b1408602eb68a4a4f3f8d26d77c0cdb24d
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2013-06-24 16:03:46 +0100

    Samplers in madx2gmad

--- parser/gmad.h
+++ parser/gmad.h
@@ -452,14 +452,44 @@ int gmad_parser(std::string name);
 extern "C" {   
   int    gmad_parser_c(char *name);
 
-  /* Interface to std::list<Element> beamline_list */
+  /* Interface to extern std::list<Element> beamline_list */
   int    get_nelements(); // Length of list
   short  get_type(int);   // Type of element 
   char*  get_name(int);   // Name of element
   double get_length(int); // Length of element
   double get_angle(int);  // Angle of element
 
-  /* Interface to Options options*/
+  /* Interface to extern Options options*/
+  /*
+  char*  get_physicsList();
+  char*  get_particleName();
+  char*  get_distribType();
+  char*  get_distribFile();
+  int    get_numberToGenerate();
+  
+  double get_sigmaX();
+  double get_sigmaY();
+  double get_sigmaXp();
+  double get_sigmaYp();
+  double get_sigmaE();
+  
+  double get_shellX();
+  double get_shellY();
+  double get_shellXp();
+  double get_shellYp();
+  
+  double get_rMin();
+  double get_rMax();
+ 
+  double get_betx();
+  double get_bety();
+  double get_alfx();
+  double get_alfy();
+  double get_emitx();
+  double get_emity();
+   
+  */
+  
   
 }
 #endif
