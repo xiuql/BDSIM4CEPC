70f394eb76794eb05f35d75c166aeb455f266577
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2013-10-16 12:49:57 +0100

    init file for pybdsim

---
+++ utils/pybdsim/__init__.py
@@ -0,0 +1,17 @@
+#pybdsim
+
+import BeamTester
+import Builder
+import ComponentTester
+import Tester
+import analysis
+import outputLoader
+
+all = ['BeamTester','Builder','ComponentTester','Tester','analysis','outputLoader']
+
+"""
+A future doc string describing the different modules within
+pybdsim
+
+sorry still to be completed...
+"""
