88e5747f147a93cdde228422f9f4fd567d0567b9
Lawrence Deacon <lawrence.deacon@ucl.ac.uk>
2013-11-25 13:45:17 +0000

    Debugginf BDSLensFacet.

--- src/BDSLensFacet.cc
+++ src/BDSLensFacet.cc
@@ -26,7 +26,8 @@ BDSLensFacet::BDSLensFacet (G4String name, G4TwoVector size, G4double angle, G4d
 }
 
 void BDSLensFacet::computeDimensions(){
-  if(_baseHeight==0) {_baseHeight=DBL_MIN*1e6;}
+  G4double aSmallNumber=1e-20;
+  if(_baseHeight==0) {_baseHeight=aSmallNumber}
   _totalHeight = _baseHeight + _size.x()*tan(_angle);
   //phi, theta, psi rotation such that the base is perp. to the z direction.
   _rotation = new G4RotationMatrix(0, BDSGlobalConstants::Instance()->GetPI()/2.0,0); 
