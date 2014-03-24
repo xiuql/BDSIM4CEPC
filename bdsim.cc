280f9dcded92c2dd0ceaf309be9633b21dd6a7c1
Lawrence Deacon <lawrence.deacon@ucl.ac.uk>
2014-03-24 16:19:32 +0000

    Changed largest acceptable step length to 1m.

--- bdsim.cc
+++ bdsim.cc
@@ -263,7 +263,7 @@ int main(int argc,char** argv) {
 
 
 
-  G4TransportationManager::GetTransportationManager()->GetPropagatorInField()->SetLargestAcceptableStep(1*CLHEP::mm);
+  G4TransportationManager::GetTransportationManager()->GetPropagatorInField()->SetLargestAcceptableStep(1*CLHEP::m);
 
   //
   // Initialize G4 kernel
