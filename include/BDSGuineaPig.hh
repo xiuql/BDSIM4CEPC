eaebc770374a2cbd66a79083526c28b4d6c8aa06
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2014-07-28 20:58:52 +0100

    Start of GP

---
+++ include/BDSGuineaPig.hh
@@ -0,0 +1,4 @@
+class BDSGuineaPig {
+  BDSGuineaPig();
+  ~BDSGuineaPig();  
+};
---
+++ src/BDSGuineaPig.cc
@@ -0,0 +1,8 @@
+#include "BDSGuineaPig.hh"
+
+// https://savannah.cern.ch/projects/guinea-pig
+// http://svnweb.cern.ch/world/wsvn/clicsw/
+
+BDSGuineaPig::BDSGuineaPig() {}
+
+BDSGuineaPig::~BDSGuineaPig() {}
