8222be4a67558d7a1a8827ec57b8692bc3bbf998
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2014-07-29 17:36:21 +0100

    completion of teleporter

--- include/BDSTeleporter.hh
+++ include/BDSTeleporter.hh
@@ -2,10 +2,11 @@
 #define BDSTeleporter_h 1
 
 #include "BDSTeleporterStepper.hh"
-#include "globals.hh"
-#include "BDSGlobalConstants.hh"
+//#include "globals.hh"
+//#include "BDSGlobalConstants.hh"
 #include "parser/elementlist.h"
 #include "BDSDrift.hh"
+#include "BDSBeamline.hh"
 
 class BDSTeleporter: public BDSDrift
 {
@@ -15,8 +16,8 @@ public:
 		  G4double apertureX,
 		  G4double apertureY,
 		  G4double phiAngleIn,
-		  G4double phiAngleOut):
-    BDSDrift(name,
+		G4double phiAngleOut);
+    /*BDSDrift(name,
 	       length,
 	       std::list<G4double>(), //blmLocZ,
 	       std::list<G4double>(), //blmLocTheta,
@@ -27,17 +28,17 @@ public:
 	       0.0,    //Similar to drift in componentfactory
 	       0.0,    //Tunnel Offset X
 	       phiAngleIn,
-	       phiAngleOut){};
+	       phiAngleOut)*/
   
   ~BDSTeleporter(){};
 
   //protected:
 private:
-  void BuildBpFieldAndStepper();
+  virtual void BuildBpFieldAndStepper();
 };
 
+void CalculateAndSetTeleporterDelta(BDSBeamline* thebeamline);
 void AddTeleporterToEndOfBeamline(ElementList* beamline_list);
-void CalculateAndSetTeleporterOffset();
-void CalculateAndSetTeleporterLength();
+
 
 #endif
--- src/BDSTeleporter.cc
+++ src/BDSTeleporter.cc
@@ -1,52 +1,87 @@
 #include "globals.hh" //G4 global constants & types
 #include "BDSGlobalConstants.hh" 
 #include "BDSDebug.hh"
-
 #include "BDSTeleporter.hh"
-
 #include "BDSMagField.hh"
 #include "G4Mag_UsualEqRhs.hh"
 #include "BDSTeleporterStepper.hh"
-
-#include "parser/element.h"
-#include "parser/elementlist.h"
+#include "G4ThreeVector.hh"
+#include "BDSBeamline.hh"
+#include <cmath>
 #include "parser/enums.h"
 
-#include "G4ThreeVector.hh"
+BDSTeleporter::BDSTeleporter(G4String name,
+			     G4double length,
+			     G4double apertureX,
+			     G4double apertureY,
+			     G4double phiAngleIn,
+			     G4double phiAngleOut):
+  BDSDrift(name,
+	   length,
+	   std::list<G4double>(), //blmLocZ,
+	   std::list<G4double>(), //blmLocTheta,
+	   apertureX,
+	   apertureY,
+	   "",     //Tunnel Material
+	   true,  //Aperture set
+	   0.0,    //Similar to drift in componentfactory
+	   0.0,    //Tunnel Offset X
+	   phiAngleIn,
+	   phiAngleOut)
+{
+  // BuildBpFieldAndStepper();
+}
 
 void BDSTeleporter::BuildBpFieldAndStepper()
 {
+  G4cout << "BDSTeleporter Build Stepper & Field " << G4endl;
   // set up the magnetic field and stepper
   itsMagField = new BDSMagField(); //Zero magnetic field.
   itsEqRhs    = new G4Mag_UsualEqRhs(itsMagField);
   itsStepper  = new BDSTeleporterStepper(itsEqRhs);
 }
 
+void CalculateAndSetTeleporterDelta(BDSBeamline* thebeamline)
+{
+  // get position of last item in beamline
+  // and then calculate necessary offset teleporter should apply
+  G4ThreeVector* lastitemposition   = thebeamline->GetLastPosition();
+  G4ThreeVector* firstitemposition  = thebeamline->GetFirstPosition();
+  G4ThreeVector  delta              = *lastitemposition/CLHEP::m - *firstitemposition/CLHEP::m;
+#ifdef DEBUG
+  G4cout << "Calculating Teleporter delta" << G4endl;
+  G4cout << "last item position  : " << *lastitemposition/CLHEP::m << G4endl;
+  G4cout << "first item position : " << *firstitemposition/CLHEP::m << G4endl;
+  G4cout << "delta               : " << delta << G4endl;
+#endif
+  BDSGlobalConstants::Instance()->SetTeleporterDelta(delta*CLHEP::m);
+  
+  // calculate length of teleporter
+  // beamline is built along z and sbend deflects in x
+  // setting length here ensures that length is always z difference
+  G4double lengthsafety           = BDSGlobalConstants::Instance()->GetLengthSafety();
+  G4double teleporterlength       = fabs(delta.z()*CLHEP::m) - lengthsafety*CLHEP::m;
+#ifdef DEBUG
+  G4cout << "Calculating teleporter length" << G4endl;
+  G4cout << "Length : " << teleporterlength << G4endl;
+#endif  
+  BDSGlobalConstants::Instance()->SetTeleporterLength(teleporterlength);
+}
+
+
+
+
 void AddTeleporterToEndOfBeamline(ElementList* beamline_list)
 {
-
 #ifdef DEBUG
-  G4cout << __FUNCTION_NAME__ << ": adding teleporter element to end of beamline" << G4endl;
+  G4cout << __METHOD_NAME__ << ": adding teleporter element to end of beamline" << G4endl;
 #endif
   //based on void add_sampler in parser.h
   //create basic element with type teleporter and put on end
   struct Element e;
   e.type = _TELEPORTER;
-  e.name = "endteleporter";
+  e.name = "Teleporter";
   e.lst  = NULL; 
-  
-beamline_list->push_back(e);
+  beamline_list->push_back(e);
 }
 
-void CalculateAndSetTeleporterOffset()
-{
-  G4ThreeVector newteleporteroffset = G4ThreeVector(0.000163*CLHEP::m,0.0,0.000119*CLHEP::m);
-  BDSGlobalConstants::Instance()->SetTeleporterOffset(newteleporteroffset);
-}
-
-void CalculateAndSetTeleporterLength()
-{
-  G4double teleporterlength = 0.112427*CLHEP::mm;
-  BDSGlobalConstants::Instance()->SetTeleporterLength(teleporterlength);
-  G4cout << "setting teleporter length to " << teleporterlength/CLHEP::mm << G4endl;
-}
