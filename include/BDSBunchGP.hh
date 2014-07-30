83f90838c4eacf524eb244127c5735b03dbd710a
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2014-07-30 13:10:17 +0100

    Started refactor of BDSBunch

--- include/BDSBunchGP.hh
+++ include/BDSBunchGP.hh
@@ -1,4 +1,4 @@
 class BDSGuineaPig {
   BDSGuineaPig();
-  ~BDSGuineaPig();  
+  ~BDSGuineaPig();
 };
---
+++ include/BDSBunchInterface.hh
@@ -0,0 +1,5 @@
+class BDSBunchInterface { 
+  virtual void GetNextParticle(G4double& x0, G4double& y0, G4double& z0, 
+			       G4double& xp, G4double& yp, G4double& zp,
+			       G4double& t , G4double&  E, G4double& weight) = 0;
+};
