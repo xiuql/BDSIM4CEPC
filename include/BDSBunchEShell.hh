1c5b3295bdf60124254b1445d47844a7047832f1
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2014-07-31 15:49:07 +0100

    Finished BDSBunchEShell

--- include/BDSBunchEShell.hh
+++ include/BDSBunchEShell.hh
@@ -1,5 +1,5 @@
-#ifndef BDSBunchERing_h
-#define BDSBunchERing_h 
+#ifndef BDSBunchEShell_h
+#define BDSBunchEShell_h 
 
 #include "BDSBunchInterface.hh"
 #include "Randomize.hh"
@@ -8,24 +8,38 @@ namespace CLHEP {
   class RandFlat;
 }
 
-class BDSBunchERing : public BDSBunchInterface { 
+class BDSBunchEShell : public BDSBunchInterface { 
+
 protected : 
-  G4double rMin;
-  G4double rMax;
+  G4double shellX;
+  G4double shellXp;
+  G4double shellY; 
+  G4double shellYp;
+
   CLHEP::RandFlat  *FlatGen;    
 
 public: 
-  BDSBunchERing();  
-  BDSBunchERing(G4double rMin,   G4double rMax,
-	       G4double  X0,     G4double Y0,    G4double Z0,   G4double T0, 
-	       G4double  Xp0,    G4double Yp0,   G4double Zp0,
-	       G4double  sigmaT, G4double sigmaE);
-  ~BDSBunchERing(); 
+  BDSBunchEShell();  
+  BDSBunchEShell(G4double shellX,  G4double shellXp,
+		 G4double shellY,  G4double shellYp,
+		 G4double X0,      G4double Y0,      G4double Z0,   G4double T0, 
+		 G4double Xp0,     G4double Yp0,     G4double Zp0,
+		 G4double sigmaT,  G4double sigmaE);
+  ~BDSBunchEShell(); 
   void SetOptions(struct Options& opt);
   void GetNextParticle(G4double& x0, G4double& y0, G4double& z0, 
 		       G4double& xp, G4double& yp, G4double& zp,
 		       G4double& t , G4double&  E, G4double& weight);
 
+  G4double GetShellX() {return shellX;}
+  G4double GetShellXp() {return shellX;}
+  G4double GetShellY() {return shellY;}
+  G4double GetShellYp() {return shellYp;}
+
+  void SetShellX(G4double shellXIn)   { shellX  = shellXIn;}
+  void SetShellXp(G4double shellXpIn) { shellXp = shellXpIn;}
+  void SetShellY(G4double shellYIn)   { shellY  = shellYIn;}
+  void SetShellYp(G4double shellYpIn) { shellYp = shellYpIn;}
 };
 
 #endif
--- src/BDSBunchEShell.cc
+++ src/BDSBunchEShell.cc
@@ -1,37 +1,56 @@
-#include "BDSBunchERing.hh"
+#include "BDSBunchEShell.hh"
 
-BDSBunchERing::BDSBunchERing() : 
-  BDSBunchInterface() {}
+BDSBunchEShell::BDSBunchEShell() : 
+  BDSBunchInterface(), shellX(0.0), shellXp(0.0), shellY(0.0), shellYp(0.0) {
+  FlatGen  = new CLHEP::RandFlat(*CLHEP::HepRandom::getTheEngine()); 
+}
 
-BDSBunchERing::BDSBunchERing() :
-  BDSBunchInterface() {}
-
-BDSBunchERing::~BDSBunchERing() : 
+BDSBunchEShell::BDSBunchEShell(G4double shellXIn, G4double shellXpIn,
+			       G4double shellYIn, G4double shellYpIn,			      
+			       G4double X0In,     G4double Y0In,       G4double Z0In,   G4double T0In, 
+			       G4double Xp0In,    G4double Yp0In,      G4double Zp0In,			     
+			       G4double sigmaTIn, G4double sigmaEIn) :
+  BDSBunchInterface(X0In,Y0In,Z0In,T0In,Xp0In,Yp0In,Zp0In,sigmaTIn,sigmaEIn), shellX(shellXIn), shellXp(shellXpIn), shellY(shellYIn), shellYp(shellYpIn)  
 {
+  FlatGen  = new CLHEP::RandFlat(*CLHEP::HepRandom::getTheEngine()); 
 }
 
-void BDSBunchRing::SetOptions(struct Options& opt) {
+BDSBunchEShell::~BDSBunchEShell() 
+{
+  delete FlatGen;
+}
+
+void BDSBunchEShell::SetOptions(struct Options& opt) {
   BDSBunchInterface::SetOptions(opt);
-  SetRMin(opt.Rmin);  
-  SetRMax(opt.Rmax);  
+  SetShellX(opt.shellX);
+  SetShellY(opt.shellY);
+  SetShellXp(opt.shellXp);
+  SetShellYp(opt.shellYp);
 }
 
-void BDSBunchRing::GetNextParticle(G4double& x0, G4double& y0, G4double& z0, 
-				       G4double& xp, G4double& yp, G4double& zp,
-				       G4double& t , G4double&  E, G4double& weight) {
-  double r = ( rMin + (rMax - rMin) *  rand() / RAND_MAX );
-  double phi = 2 * CLHEP::pi * rand() / RAND_MAX;
-     
-  x0 = ( X0 + r * sin(phi) ) * CLHEP::m;
-  y0 = ( Y0 + r * cos(phi) ) * CLHEP::m;
+void BDSBunchEShell::GetNextParticle(G4double& x0, G4double& y0, G4double& z0, 
+				    G4double& xp, G4double& yp, G4double& zp,
+				    G4double& t , G4double&  E, G4double& weight) {
+  G4double phi = 2 * CLHEP::pi * FlatGen->shoot();
+  
+  x0 = (X0 + sin(phi) * shellX) * CLHEP::m;
+  xp = Xp0 + cos(phi) * shellXp;
+  
+  phi = 2 * CLHEP::pi * FlatGen->shoot();
+  
+  y0 = (Y0 + sin(phi) * shellY) * CLHEP::m;
+  yp = Yp0 + cos(phi) * shellYp;
+  
   z0 = Z0 * CLHEP::m;
-  xp = Xp0 * CLHEP::rad;
-  yp = Yp0 * CLHEP::rad;
   if (Zp0<0)
     zp = -sqrt(1.-xp*xp -yp*yp);
   else
     zp = sqrt(1.-xp*xp -yp*yp);
+  
   t = T0 * CLHEP::s;
-  E = BDSGlobalConstants::Instance()->GetBeamKineticEnergy()* (1 + sigmaE/2. * (1. -2. * FlatGen->shoot()));  
+  E = BDSGlobalConstants::Instance()->GetBeamKineticEnergy()* (1 + sigmaE/2. * (1. -2. * FlatGen->shoot()));
   weight = 1.0;
+
+  return;
 }
+		    
