41a97e34282a5f3ddf2d2e99f9d4a11f602c9ed1
Jochem Snuverink <Jochem.Snuverink@rhul.ac.uk>
2014-08-05 12:08:56 +0100

    add option to skip certain values; add PLACET test; add Decapole test

--- src/BDSBunchUserFile.cc
+++ src/BDSBunchUserFile.cc
@@ -42,10 +42,10 @@ void BDSBunchUserFile::ParseFileFormat(){
     
     unparsed_str = unparsed_str.substr(pos+1);
 #ifdef DEBUG 
-    G4cout<< "BDSBunch : " <<"token ->"<<token<<G4endl;
+    G4cout<< "BDSBunch : " <<"token -> "<<token<<G4endl;
     G4cout<< "BDSBunch : token.substr(0,1) -> " << token.substr(0,1) << G4endl;
-    G4cout<< "BDSBunch : " <<"unparsed_str ->"<<unparsed_str<<G4endl;
-    G4cout<< "BDSBunch : " <<"pos ->"<<pos<<G4endl;
+    G4cout<< "BDSBunch : " <<"unparsed_str -> "<<unparsed_str<<G4endl;
+    G4cout<< "BDSBunch : " <<"pos -> "<<pos<<G4endl;
 #endif
     if(token.substr(0,1)=="E") {
 #ifdef DEBUG 
@@ -300,6 +300,14 @@ void BDSBunchUserFile::ParseFileFormat(){
       sd.name="weight";
       sd.unit=1;
       fields.push_back(sd);
+    } else if (token.substr(0,1)=="-") {
+      // skip
+#ifdef DEBUG 
+      G4cout<< "BDSBunch : skip token " <<G4endl;
+#endif
+      sd.name="skip";
+      sd.unit=1;
+      fields.push_back(sd);
     } else {
       G4cerr << "Cannot determine bunch data format" << G4endl; exit(1);
     }
@@ -356,8 +364,8 @@ void BDSBunchUserFile::GetNextParticle(G4double& x0, G4double& y0, G4double& z0,
 	G4cout<< "BDSBunch : " << E <<G4endl;
 #endif
       }
-      G4double P=0;
-      if(it->name=="P") { 
+      else if(it->name=="P") { 
+	G4double P=0;
 	ReadValue(P); P *= ( CLHEP::GeV * it->unit ); //Paticle momentum
 	G4double particleMass = BDSGlobalConstants::Instance()->GetParticleDefinition()->GetPDGMass();
 	G4double totalEnergy = sqrt(P*P + particleMass*particleMass);
@@ -369,28 +377,28 @@ void BDSBunchUserFile::GetNextParticle(G4double& x0, G4double& y0, G4double& z0,
 	G4cout<< "BDSBunch : " << E <<G4endl;
 #endif
       }
-      if(it->name=="t") { ReadValue(t); t *= ( CLHEP::s * it->unit ); tdef = true; }
-      if(it->name=="x") { ReadValue(x0); x0 *= ( CLHEP::m * it->unit ); 
+      else if(it->name=="t") { ReadValue(t); t *= ( CLHEP::s * it->unit ); tdef = true; }
+      else if(it->name=="x") { ReadValue(x0); x0 *= ( CLHEP::m * it->unit ); 
 #ifdef DEBUG 
 	G4cout<< "BDSBunch : " << x0 <<G4endl;
 #endif
       }
-      if(it->name=="y") { 
+      else if(it->name=="y") { 
 	ReadValue(y0); y0 *= ( CLHEP::m * it->unit ); 
 #ifdef DEBUG 
 	G4cout<< "BDSBunch : " << y0 <<G4endl;
 #endif
       }
-      if(it->name=="z") { 
+      else if(it->name=="z") { 
 	ReadValue(z0); z0 *= ( CLHEP::m * it->unit ); 
 #ifdef DEBUG 
 	G4cout<< "BDSBunch : " << z0 <<G4endl;
 #endif
       }
-      if(it->name=="xp") { ReadValue(xp); xp *= ( CLHEP::radian * it->unit ); }
-      if(it->name=="yp") { ReadValue(yp); yp *= ( CLHEP::radian * it->unit ); }
-      if(it->name=="zp") { ReadValue(zp); zp *= ( CLHEP::radian * it->unit ); zpdef = true;}
-      if(it->name=="pt") {
+      else if(it->name=="xp") { ReadValue(xp); xp *= ( CLHEP::radian * it->unit ); }
+      else if(it->name=="yp") { ReadValue(yp); yp *= ( CLHEP::radian * it->unit ); }
+      else if(it->name=="zp") { ReadValue(zp); zp *= ( CLHEP::radian * it->unit ); zpdef = true;}
+      else if(it->name=="pt") {
 	ReadValue(type);
 	if(InputBunchFile.good()){
 	  BDSGlobalConstants::Instance()->SetParticleName(G4ParticleTable::GetParticleTable()->FindParticle(type)->GetParticleName());
@@ -404,7 +412,9 @@ void BDSBunchUserFile::GetNextParticle(G4double& x0, G4double& y0, G4double& z0,
 	    }
 	}
       }
-      if(it->name=="weight") ReadValue(weight);
+      else if(it->name=="weight") ReadValue(weight);
+      
+      else if(it->name=="skip") {double dummy; ReadValue(dummy);}
       
       // compute zp from xp and yp if it hasn't been read from file
       if (!zpdef) zp=sqrt(1.-xp*xp -yp*yp);
