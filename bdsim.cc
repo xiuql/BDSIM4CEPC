f7cbbbc8e3eb93b98900acbd94fe83756a2fd56d
Lawrence Deacon <lawrence.deacon@ucl.ac.uk>
2014-08-15 07:37:07 +0100

    Disabled modular physics list

--- bdsim.cc
+++ bdsim.cc
@@ -48,7 +48,7 @@
 #include "BDSDetectorConstruction.hh"   
 #include "BDSEventAction.hh"
 #include "BDSPhysicsList.hh"
-#include "BDSModularPhysicsList.hh"
+#include "BDSPhysicsList.hh"
 #include "QGSP_BERT.hh"
 #include "QGSP_BERT_HP.hh"
 #include "BDSPrimaryGeneratorAction.hh"
@@ -188,7 +188,7 @@ int main(int argc,char** argv) {
   G4cout << __FUNCTION__ << "> Constructing phys list" << G4endl;
 #endif
 
-  BDSModularPhysicsList* PhysList=new BDSModularPhysicsList;
+  BDSPhysicsList* PhysList=new BDSPhysicsList;
   runManager->SetUserInitialization(PhysList);
   
 #ifdef BDSDEBUG 
