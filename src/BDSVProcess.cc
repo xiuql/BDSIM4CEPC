3e68e827aa5293fc45406a00dcdf55c09200ab35
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2014-08-15 10:32:24 +0100

    fix so it compiles on clang - ok on gcc but not on clang http://tinyurl.com/osf2ud4

--- src/BDSVProcess.cc
+++ src/BDSVProcess.cc
@@ -1,6 +1,6 @@
 #include "BDSVProcess.hh"
 
-BDSVProcess::BDSVProcess(const G4String &aName="NoName", G4ProcessType aType=fNotDefined):G4VProcess(aName, aType){;}
+BDSVProcess::BDSVProcess(const G4String &aName, G4ProcessType aType):G4VProcess(aName, aType){;}
 
 BDSVProcess::BDSVProcess(const BDSVProcess &right):G4VProcess(right){;}
 
