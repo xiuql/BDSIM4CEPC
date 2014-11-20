8ad629f42d74efe7b5899aea4b22ed4b759d01d7
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2014-11-20 21:49:29 +0000

    Implementation of robdsimOutput

---
+++ utils/robdsim/robdsimOutput.C
@@ -0,0 +1,26 @@
+#include "robdsimOutput.h"
+
+robdsimOutput::robdsimOutput() {
+}
+
+robdsimOutput::robdsimOutput(char *path) {
+}
+
+robdsimOutput::~robdsimOutput() {
+
+}
+
+void robdsimOutput::Chain() {
+}
+  
+void robdsimOutput::ElossLoop() {
+}
+ 
+void robdsimOutput::PlossLoop() {
+}
+
+void robdsimOutput::PrecisionElossLoop() {
+}
+
+void robdsimOutput::SamplerLoop() {
+}
--- utils/robdsim/robdsimOutput.h
+++ utils/robdsim/robdsimOutput.h
@@ -1,5 +1,15 @@
 #ifndef __RoBdsimOutput_h
 
+#include <vector>
+
+#include "TChain.h"
+#include "TTree.h"
+#include "TFile.h"
+
+#include "Sampler.h"
+#include "Eloss.h"
+#include "PrecisionEloss.h"
+
 class robdsimOutput {
 
  public : 
@@ -12,11 +22,22 @@ class robdsimOutput {
   void ElossLoop(); 
   void PlossLoop();
   void PrecisionElossLoop();
+  void SamplerLoop(); 
 
  private: 
   char *path; 
+  TChain *primaryChain; 
+  TChain *elossChain;
+  TChain *plossChain;
+  TChain *precisionElossChain;
+  std::vector<TChain*> samplerChains; 
 
-    
+  Sampler        primary; 
+  Sampler        sampler; 
+  Eloss          eloss;
+  Eloss          ploss;
+  PrecisionEloss peloss;
+
 };
 
 #endif
