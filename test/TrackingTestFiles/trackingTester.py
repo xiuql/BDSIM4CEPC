a627927a7c2d5c7b95edb28405306ac48e673dfa
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2014-12-14 22:10:15 -0800

    Added beams to test generator

--- test/TrackingTestFiles/trackingTester.py
+++ test/TrackingTestFiles/trackingTester.py
@@ -1,21 +1,38 @@
+import pymadx.Ptc
+import pymadx.Beam
 import pymadx.Builder
+import pybdsim.Beam
 import pybdsim.Builder 
 
-def makeTest(type, name, **kwargs) : 
+def makeTest(type, name, particle, energy,**kwargs) : 
     print 'makeTest.type> ',type
     print 'makeTest.name> ',name
     for k in kwargs : 
         print 'makeTest.kwargs>',k+'='+str(kwargs[k])
 
-    bb = pybdsim.Builder.Machine()
-    xb = pymadx.Builder.Machine()
+    ptcFileName = 'inrays.madx'
+
+    ptc = pymadx.Ptc.Generator()
+    ptc.generate(1000,ptcFileName)
+
+    bb  = pybdsim.Beam(particle,energy,'ptc')
+    xb  = pymadx.Beam(particle,energy,'ptc')
+
+    bb.SetDistribFileName(ptcFileName) 
+    xb.SetDistribFileName(ptcFileName) 
+
+    bm  = pybdsim.Builder.Machine()
+    xm  = pymadx.Builder.Machine()
+
+    bm.AddBeam(bb)
+    xm.AddBeam(xb)
 
     if type == 'drift' : 
-        bb.AddDrift(name,kwargs['l'])
-        xb.AddDrift(name,kwargs['l'])
+        bm.AddDrift(name,kwargs['l'])
+        xm.AddDrift(name,kwargs['l'])
 
-    bb.AddSampler('all')
-    xb.AddSampler('all')
+    bm.AddSampler('all')
+    xm.AddSampler('all')
         
-    bb.WriteLattice(type)
-    xb.WriteLattice(type)
+    bm.WriteLattice(type)
+    xm.WriteLattice(type)
