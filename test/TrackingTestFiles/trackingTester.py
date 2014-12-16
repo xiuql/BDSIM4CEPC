46f53ef8da06ade68fd160332debff82d82276ab
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2014-12-16 09:52:49 -0800

    tracking testing almost complete

--- test/TrackingTestFiles/trackingTester.py
+++ test/TrackingTestFiles/trackingTester.py
@@ -3,6 +3,23 @@ import pymadx.Beam
 import pymadx.Builder
 import pybdsim.Beam
 import pybdsim.Builder 
+import os as _os
+
+def test(type,name="test",particle="e-",energy=1.0,**kwargs) : 
+    '''execute test for type
+    calls : cleanTest, makeTest, executeTest, analysisTest
+    '''
+    cleanTest(type)
+    makeTest(type,name,particle,energy,**kwargs)
+    executeTest(type)
+    
+def cleanTest(type) : 
+    '''Remove all files associated with test'''
+
+    _os.system("rm -rf "+type+"*")
+    _os.system("rm -rf output*")
+    _os.system("rm -rf Maxwellian_bend_for_ptc.txt trackone inrays.madx") 
+
 
 def makeTest(type, name, particle, energy,**kwargs) : 
     print 'makeTest.type> ',type
@@ -28,14 +45,22 @@ def makeTest(type, name, particle, energy,**kwargs) :
     xm.AddBeam(xb)
 
     if type == 'drift' : 
-        bm.AddDrift(name,kwargs['l'])
-        xm.AddDrift(name,kwargs['l'])
+        bm.AddDrift(name,**kwargs)
+        xm.AddDrift(name,**kwargs)
     elif type == 'quadrupole' : 
-        bm.AddQuadrupole(name,length=kwargs['length'],k1=kwargs['k1'])
-        xm.AddQuadrupole(name,length=kwargs['length'],k1=kwargs['k1'])
+        bm.AddQuadrupole(name,**kwargs)
+        xm.AddQuadrupole(name,**kwargs)
+
+    bm.AddMarker("theend") # Need a post element marker to sample at, only for bdsim
 
     bm.AddSampler('all')
     xm.AddSampler('all')
         
     bm.WriteLattice(type)
     xm.WriteLattice(type)
+
+def executeTest(type) : 
+    _os.system("madx_dev < "+type+".madx > madx.log")
+    _os.system("bdsim --file="+type+".gmad --batch --output=root > madx.log")
+
+
