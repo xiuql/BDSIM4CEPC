74837030ad0aac7951fc144f76e268a6b3e17c8c
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2014-12-17 11:34:21 -0800

    Some more inray files (px,y)

--- test/TrackingTestFiles/ThinMagnet/compare.py
+++ test/TrackingTestFiles/ThinMagnet/compare.py
@@ -1,12 +1,31 @@
+import pylab as _pl
+import numpy as _np
 import pymadx
 import robdsim
 
-def compare(column, madxFile='trackone', bdsimFile='output_0.root') : 
+def compare(column = 1, madxFile='trackone', bdsimFile='output_0.root') : 
     m = pymadx.TfsArray(madxFile) 
     r = robdsim.robdsimOutput(bdsimFile) 
     
-    md = ((m.GetSegment(5)).dataArray)[:,2:]
-    rd = robdsim.d2darrayToNumpy(r.GetSamplerData(4))
+    md = ((m.GetSegment(5)).dataArray)[:,3:-2]
+    print m.GetSegment(5).columns
+    rd = robdsim.d2darrayToNumpy(r.GetSamplerData(4))[:-1,:] # have an extra particle in root !
+
+    # fix root units back to metres 
+    rd[:,0] = rd[:,0]/1000.0/1000.0
+    rd[:,2] = rd[:,2]/1000.0/1000.0
     
-    print shape(md)
-    print shape(rd)
+    print _np.shape(md)
+    print _np.shape(rd)
+
+    # plot
+    _pl.figure(1)
+    _pl.clf()
+    
+    _pl.plot(md[:,column],rd[:,column],"+")
+
+    # straight line on top
+    x = md[:,column]
+    y = x 
+
+    _pl.plot(x,y)
