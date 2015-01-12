2d1cf9a3c40f421a3ed38e6ebbf69efb5df42306
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2015-01-12 20:39:43 +0000

    make tracking test into class, include flat generator for inputs

--- test/TrackingTestFiles/trackingTester.py
+++ test/TrackingTestFiles/trackingTester.py
@@ -5,76 +5,96 @@ import pybdsim.Beam
 import pybdsim.Builder 
 import os as _os
 
-def test(type_,name="test",particle="e-",energy=1.0,**kwargs) : 
-    '''execute test for type
-    calls : cleanTest, makeTest, executeTest, analysisTest
-    '''
-    cleanTest(type_)
-    makeTest(type_,name,particle,energy,**kwargs)
-    executeTest(type_)
+class Test:
+    def __init__(self,type_,foldername=None,particle="e-",energy=1.0,distribution='flat',nparticles=10,**kwargs): 
+        """
+        Tracking test class
+
+        type = 'drift' | 'quadrupole' | 'sextupole'
+        distribution = 'flat' | 'gaussian'
+        """
+        self.type_        = type_
+        self.filename     = self.type_
+        self.foldername   = foldername
+        self.ptcfilename  = 'inrays.madx'
+        if foldername != None:
+            self.usingfolder = True
+            self.filepath = self.foldername+'/'+self.filename
+            self.ptcfilepath =  foldername+'/'+self.ptcfilename
+        else:
+            self.usingfolder = False
+            self.filepath = self.filename
+            self.ptcfilepath = self.ptcfilename
+        self.particle     = particle
+        self.energy       = energy
+        self.distribution = distribution
+        self.nparticles   = nparticles
+        self.kwargs       = kwargs
+        
+    def CleanMakeRun(self):
+        self.Clean()
+        self.Make()
+        self.Execute()
     
-def cleanTest(type_) : 
-    '''Remove all files associated with test'''
+    def Clean(self):        
+        _os.system("rm -rf "+self.filepath+"*")
+        _os.system("rm -rf output*")
+        _os.system("rm -rf Maxwellian_bend_for_ptc.txt trackone inrays.madx") 
+        
+    def Make(self):
+        #type_='drift', foldername=None, particle='e-', energy=1.0,**kwargs) : 
+        print 'Test> Element type:         ',self.type_
+        print 'Test> Destination filepath: ',self.filepath
+        print 'Test> kwargs: ',
+        for k in self.kwargs : 
+            print k+'='+str(self.kwargs[k]),
 
-    _os.system("rm -rf "+type_+"*")
-    _os.system("rm -rf output*")
-    _os.system("rm -rf Maxwellian_bend_for_ptc.txt trackone inrays.madx") 
+        if self.distribution == 'flat':
+            ptc = pymadx.Ptc.FlatGenerator()
+        elif self.distribution == 'gauss':
+            ptc = pymadx.Ptc.GaussGenerator()
+        
+        ptc.Generate(self.nparticles,self.ptcfilepath)
+        
+        bb  = pybdsim.Beam(self.particle,self.energy,'ptc')
+        xb  = pymadx.Beam(self.particle,self.energy,'ptc')
+        
+        bb.SetDistribFileName(self.ptcfilename) 
+        xb.SetDistribFileName(self.ptcfilename) 
+        
+        bm  = pybdsim.Builder.Machine()
+        xm  = pymadx.Builder.Machine()
+        
+        bm.AddBeam(bb)
+        xm.AddBeam(xb)
+    
+        if self.type_ == 'drift' :
+            name = 'd1'
+            bm.AddDrift(name,**self.kwargs)
+            xm.AddDrift(name,**self.kwargs)
+        elif self.type_ == 'quadrupole' :
+            name = 'q1'
+            bm.AddQuadrupole(name,**self.kwargs)
+            xm.AddQuadrupole(name,**self.kwargs)
+        elif self.type_ == 'sextupole' :
+            name = 's1'
+            bm.AddSextupole(name,**self.kwargs)
+            xm.AddSextupole(name,**self.kwargs)        
 
+        bm.AddMarker("theend") # Need a post element marker to sample at, only for bdsim
+        
+        bm.AddSampler('theend')
+        xm.AddSampler('all')
 
-def makeTest(type_='drift', foldername=None, particle='e-', energy=1.0,**kwargs) : 
-    print 'makeTest.type>   ',type_
-    print 'makeTest.folder> ',foldername
-    for k in kwargs : 
-        print 'makeTest.kwargs> ',k+'='+str(kwargs[k])
-
-    ptcFileName = 'inrays.madx'
-    if foldername != None:
-        ptcPathName = foldername+'/'+ptcFileName
-    else:
-        ptcPathName = ptcFileName
-
-    ptc = pymadx.Ptc.Generator()
-    ptc.Generate(1000,ptcPathName)
-
-    bb  = pybdsim.Beam(particle,energy,'ptc')
-    xb  = pymadx.Beam(particle,energy,'ptc')
-
-    bb.SetDistribFileName(ptcFileName) 
-    xb.SetDistribFileName(ptcFileName) 
-
-    bm  = pybdsim.Builder.Machine()
-    xm  = pymadx.Builder.Machine()
-
-    bm.AddBeam(bb)
-    xm.AddBeam(xb)
-
-    if type_ == 'drift' :
-        name = 'd1'
-        bm.AddDrift(name,**kwargs)
-        xm.AddDrift(name,**kwargs)
-    elif type_ == 'quadrupole' :
-        name = 'q1'
-        bm.AddQuadrupole(name,**kwargs)
-        xm.AddQuadrupole(name,**kwargs)
-    elif type == 'sextupole' :
-        name = 's1'
-        bm.AddSextupole(name,**kwargs)
-        xm.AddSextupole(name,**kwargs)        
-
-    bm.AddMarker("theend") # Need a post element marker to sample at, only for bdsim
-
-    bm.AddSampler('all')
-    xm.AddSampler('all')
-
-    if foldername != None:
-        outputname = foldername+'/'+type_
-    else:
-        outputname = type_
-    bm.WriteLattice(outputname)
-    xm.WriteLattice(outputname)
-
-def executeTest(type_) : 
-    _os.system("madx < "+type_+".madx > madx.log")
-    _os.system("bdsim --file="+type_+".gmad --batch --output=root > bdsim.log")
-
-
+        bm.WriteLattice(self.filepath)
+        xm.WriteLattice(self.filepath)
+            
+    def Execute(self):
+        if self.usingfolder:
+            _os.chdir(self.foldername)
+        
+        _os.system("madx < "+self.filename+".madx > madx.log")
+        _os.system("bdsim --file="+self.filename+".gmad --batch --output=root --outfile='test' > bdsim.log")
+        
+        if self.usingfolder:
+            _os.chdir("../")
