09cf6f37316065fdcb89c1bd5fb711fe16dfe7f7
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2015-01-23 09:49:34 +0100

    make marker volumes more transparent in debug mode

--- src/BDSDetectorConstruction.cc
+++ src/BDSDetectorConstruction.cc
@@ -595,7 +595,7 @@ void BDSDetectorConstruction::ComponentPlacement(){
       G4LogicalVolume* LocalLogVol = thecurrentitem->GetMarkerLogicalVolume();
       G4String LogVolName          = LocalLogVol->GetName();
       // Set visualisation options for marker volumes - perhaps should be in base class..
-      static G4VisAttributes* VisAtt1 = new G4VisAttributes(G4Colour(0.0, 1.0, 0.0, 0.4));
+      static G4VisAttributes* VisAtt1 = new G4VisAttributes(G4Colour(0.0, 1.0, 0.0, 0.1));
       VisAtt1->SetForceSolid(true);  
       // Set visible only if debug build, otherwise hidden
 #if defined BDSDEBUG
