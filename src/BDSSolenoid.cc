b8e7a9ce4be99788e2da29b34e4afae4c4873074
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2015-02-09 14:54:19 +0000

    set sensible step size limit

--- src/BDSSolenoid.cc
+++ src/BDSSolenoid.cc
@@ -42,7 +42,7 @@ void BDSSolenoid::Build()
 void BDSSolenoid::BuildBeampipe(G4String materialName)
 {
   BDSMultipole::BuildBeampipe(materialName);
-  itsInnerBeampipeUserLimits->SetMaxAllowedStep(itsLength*0.01);
+  itsInnerBeampipeUserLimits->SetMaxAllowedStep(itsLength*0.5);
   itsInnerBPLogicalVolume->SetUserLimits(itsInnerBeampipeUserLimits);
 }
 
