b8cd1d9541701c0bf02501342842652381e02dcc
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2015-02-10 22:22:26 +0000

    documentation

--- include/BDSBeamPipeType.hh
+++ include/BDSBeamPipeType.hh
@@ -3,6 +3,13 @@
 
 #include "BDSTypeSafeEnum.hh"
 
+/**
+ * @brief type definition for beampipes - used for comparison
+ * in factory methods
+ * 
+ * @author Laurie Nevay <laurie.nevay@rhul.ac.uk>
+ */
+
 struct beampipetypes_def {
   enum type { circular, elliptical, rectangular };
 };
