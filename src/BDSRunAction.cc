f9463627d57dafb6ea9ed5a939b26e0f9df85c84
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2015-02-13 22:01:30 +0000

    register new histograms in beginning of run action

--- src/BDSRunAction.cc
+++ src/BDSRunAction.cc
@@ -1,57 +1,59 @@
-/* BDSIM code.    Version 1.0
-   Author: Grahame A. Blair, Royal Holloway, Univ. of London.
-   Last modified 24.7.2002
-   Copyright (c) 2002 by G.A.Blair.  ALL RIGHTS RESERVED. 
-*/
-
-//==========================================================
-//==========================================================
-
 #include "BDSExecOptions.hh"
 #include "BDSGlobalConstants.hh" 
 #include "BDSOutputBase.hh" 
 #include "BDSRunAction.hh"
 #include "BDSRunManager.hh"
 #include "BDSDebug.hh"
-
+#include "BDSAnalysisManager.hh"
+#include "BDSBeamline.hh"
 #include "G4Run.hh"
-//#include "G4UImanager.hh"
-//#include "G4VVisManager.hh"
-#include "G4ios.hh"
+
+#include "globals.hh"               // geant4 globals / types
 
 extern BDSOutputBase* bdsOutput;         // output interface
 
-//==========================================================
-
 BDSRunAction::BDSRunAction()
-{
-}
-
-//==========================================================
+{}
 
 BDSRunAction::~BDSRunAction()
 {}
-
-//==========================================================
 
 void BDSRunAction::BeginOfRunAction(const G4Run* aRun)
 {
   //Get the current time
   starttime = time(NULL);
 
+  // construct output histograms
+  // calculate histogram dimensions
+  G4double smin, smax, binwidth, nbins;
+  smin     = 0.0;
+  smax     = BDSGlobalConstants::Instance()->GetSMax()/CLHEP::m;
+  binwidth = BDSGlobalConstants::Instance()->GetElossHistoBinWidth(); // CHECK UNITS
+  nbins    = (int) ceil((smax-smin)/binwidth); // rounding up so last bin definitely covers smax
+  smax     = smin + (nbins*binwidth);          // redefine smax
+  // create the histograms
+  phitsindex = BDSAnalysisManager::Instance()->Create1DHistogram("phits","Primary Hits",nbins,smin,smax); //0
+  plossindex = BDSAnalysisManager::Instance()->Create1DHistogram("ploss","Primary Loss",nbins,smin,smax); //1
+  elossindex = BDSAnalysisManager::Instance()->Create1DHistogram("eloss","Energy Loss", nbins,smin,smax); //2
+  // prepare bin edges for a by-element histogram
+  std::vector<double> binedges;
+  binedges.push_back(0.0);
+  G4double s_end = 0.0; // s position at the end of the element
+  for (BDSBeamline::Instance()->first(); !BDSBeamline::Instance()->isDone(); BDSBeamline::Instance()->next())
+    {
+      BDSAcceleratorComponent* item = BDSBeamline::Instance()->currentItem();
+      s_end += item->GetArcLength()/CLHEP::m; // use arc length as hits binned in S
+      binedges.push_back(s_end);
+    }
+  // create per element ("pe") bin width histograms
+  phitspeindex = BDSAnalysisManager::Instance()->Create1DHistogram("phitspe","Primary Hits per Element",binedges); //3
+  plosspeindex = BDSAnalysisManager::Instance()->Create1DHistogram("plosspe","Primary Loss per Element",binedges); //4
+  elosspeindex = BDSAnalysisManager::Instance()->Create1DHistogram("elosspe","Energy Loss per Element" ,binedges); //5
+  
   //Output feedback
   G4cout << __METHOD_NAME__ << " Run " << aRun->GetRunID() << " start. Time is " << asctime(localtime(&starttime)) << G4endl;
 
-  //  if (G4VVisManager::GetConcreteInstance())
-  //    {
-      //      G4UImanager* UI = G4UImanager::GetUIpointer(); 
-      //  UI->ApplyCommand("/vis/scene/notifyHandlers");
-  //    } 
-
-
 }
-
-//==========================================================
 
 void BDSRunAction::EndOfRunAction(const G4Run* aRun)
 {
