256fb6f3e7007eee222a487b64059452400d7346
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2015-02-13 20:28:16 +0000

    introducing - BDSAnalysisManager - singleton manager for histograms - being of factorising histogramming / analysis from output classes

---
+++ include/BDSAnalysisManager.hh
@@ -0,0 +1,56 @@
+#ifndef BDSANALYSISMANAGER_H
+#define BDSANALYSISMANAGER_H
+
+#include "BDSHistogram.hh"
+#include "globals.hh"         // geant4 globals / types
+#include <vector>
+
+/**
+ * @brief Analysis interface class. Create, store and access
+ * histograms.
+ * 
+ * singleton pattern
+ */
+
+class BDSAnalysisManager
+{
+public:
+  /// singleton accessor
+  static BDSAnalysisManager* Instance();
+  ~BDSAnalysisManager();
+
+  /// Create a new histogram
+  G4int Create1DHistogram(G4String name,
+			  G4String title,
+			  G4int    nbins,
+			  G4double xmin,
+			  G4double xmax);
+
+  G4int Create1DHistogram(G4String name,
+			  G4String title,
+			  std::vector<double> edges);
+
+  /// Access a histogram
+  BDSHistogram1D* GetHistogram(G4int index);
+
+  /// Fill a histogram
+  void Fill1DHistogram(G4int histoIndex, G4double value, G4double weight=1.0);
+  
+  
+
+private:
+  /// private default constructor for singleton pattern
+  BDSAnalysisManager();
+
+  /// function to check whether histogram index is valid
+  void CheckHistogramIndex(G4int index);
+  
+  static BDSAnalysisManager* _instance;
+
+  std::vector<BDSHistogram1D*> histograms1d;
+  
+};
+
+#endif
+
+  
---
+++ src/BDSAnalysisManager.cc
@@ -0,0 +1,70 @@
+#include "BDSAnalysisManager.hh"
+#include "BDSDebug.hh"
+#include "BDSHistogram.hh"
+#include "globals.hh"             // geant4 globals / types
+
+#include <vector>
+#include <iterator>
+
+BDSAnalysisManager* BDSAnalysisManager::_instance = 0;
+
+BDSAnalysisManager* BDSAnalysisManager::Instance()
+{
+  if (_instance == 0)
+    {
+      _instance = new BDSAnalysisManager();
+    }
+  return _instance;
+}
+
+BDSAnalysisManager::BDSAnalysisManager(){;}
+
+BDSAnalysisManager::~BDSAnalysisManager()
+{
+  //delete all the 1d histograms created
+  std::vector<BDSHistogram1D*>::iterator iter, end;
+  for (iter = histograms1d.begin(), end = histograms1d.end(); iter != end; ++iter)
+    {
+      delete (*iter);
+    }
+}
+
+G4int BDSAnalysisManager::Create1DHistogram(G4String name,
+					    G4String /*title*/,
+					    G4int    nbins,
+					    G4double xmin,
+					    G4double xmax)
+{
+  histograms1d.push_back(new BDSHistogram1D(xmin, xmax, nbins, name));
+  return histograms1d.size();
+}
+
+G4int BDSAnalysisManager::Create1DHistogram(G4String name,
+					    G4String /*title*/,
+					    std::vector<double> edges)
+{
+  histograms1d.push_back(new BDSHistogram1D(edges,name));
+  return histograms1d.size();
+}
+
+BDSHistogram1D* BDSAnalysisManager::GetHistogram(G4int histoIndex)
+{
+  CheckHistogramIndex(histoIndex);
+  return histograms1d[histoIndex];
+}
+
+void BDSAnalysisManager::Fill1DHistogram(G4int histoIndex, G4double value, G4double weight)
+{
+  CheckHistogramIndex(histoIndex);
+  histograms1d[histoIndex]->Fill(value,weight);
+}
+
+void  BDSAnalysisManager::CheckHistogramIndex(G4int histoIndex)
+{
+  if ( ((size_t)histoIndex > histograms1d.size()) || histoIndex < 0 )
+  {
+    G4cerr << __METHOD_NAME__ << "invalid histogram index" << G4endl;
+    exit(1);
+  }
+  // void - keep going
+}
