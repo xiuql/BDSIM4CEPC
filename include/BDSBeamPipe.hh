acea27132dd003e2dea312913a118302a6ad409c
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2015-02-27 13:04:40 +0000

    include an extra solid for container subtraction in an accelerator component - ie one that's longer than the container for unambiguous subtraction - can ONLY be used for transverse subtraction really as it's not the true length of the item - this is why it's in beampipe and not in bdsgeometrycomponent

--- include/BDSBeamPipe.hh
+++ include/BDSBeamPipe.hh
@@ -5,6 +5,8 @@
 
 #include "globals.hh"               // geant4 globals / types
 #include "G4LogicalVolume.hh"
+#include "G4VSolid.hh"
+#include <utility>                  // for std::pair
 
 /**
  * @brief A holder class for all information required for a
@@ -17,17 +19,23 @@
 class BDSBeamPipe: public BDSGeometryComponent
 {
 public:
+  /// constructor has BDSGeometryComponent members first,
+  /// then everything extra for this derived class
   BDSBeamPipe(G4VSolid*                 containerSolidIn,
 	      G4LogicalVolume*          containerLVIn,
 	      std::pair<double, double> extentXIn,
 	      std::pair<double, double> extentYIn,
 	      std::pair<double, double> extentZIn,
+	      G4VSolid*                 containerSubtractionSolidIn,
 	      G4LogicalVolume*          vacuumLVIn,
 	      G4bool                    containerIsCircularIn = false,
 	      G4double                  containerRadiusIn = 0.0
 	      );
   ~BDSBeamPipe(); /// default destructor sufficient as G4 manages solids and LVs
 
+  /// access a solid for beampipe subtraction - note this is typically longer
+  /// than the actual beampipe for unambiguous subtraction
+  G4VSolid*        GetContainerSubtractionSolid();
   /// access the vacuum volume to set fields and limits
   G4LogicalVolume* GetVacuumLogicalVolume();
   /// flag to tell whether the parent volume needn't use a subtraction
@@ -37,10 +45,14 @@ public:
   G4double         GetContainerRadius();
   
 protected:
+  G4VSolid*        containerSubtractionSolid;
   G4LogicalVolume* vacuumLogicalVolume;
   G4bool           containerIsCircular;
   G4double         containerRadius;
 };
+
+inline G4VSolid* BDSBeamPipe::GetContainerSubtractionSolid()
+{return containerSubtractionSolid;}
 
 inline G4LogicalVolume* BDSBeamPipe::GetVacuumLogicalVolume()
 {return vacuumLogicalVolume;}
--- src/BDSBeamPipe.cc
+++ src/BDSBeamPipe.cc
@@ -10,11 +10,14 @@ BDSBeamPipe::BDSBeamPipe(G4VSolid*                 containerSolidIn,
 			 std::pair<double, double> extentXIn,
 			 std::pair<double, double> extentYIn,
 			 std::pair<double, double> extentZIn,
+			 G4VSolid*                 containerSubtractionSolidIn,
 			 G4LogicalVolume*          vacuumLVIn,
 			 G4bool                    containerIsCircularIn,
 			 G4double                  containerRadiusIn
 			 ):
   BDSGeometryComponent(containerSolidIn, containerLVIn, extentXIn, extentYIn, extentZIn),
-  vacuumLogicalVolume(vacuumLVIn), containerIsCircular(containerIsCircularIn),
+  containerSubtractionSolid(containerSubtractionSolidIn),
+  vacuumLogicalVolume(vacuumLVIn),
+  containerIsCircular(containerIsCircularIn),
   containerRadius(containerRadiusIn)
 {;}
