6add37a2a86e98cfda5a0c34ef7506b4388fc0ab
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2015-02-27 13:15:37 +0000

    debug output for beampipe type and new string comparison method as old one didn't work

--- src/BDSBeamPipeType.cc
+++ src/BDSBeamPipeType.cc
@@ -1,17 +1,38 @@
 #include "BDSBeamPipeType.hh"
-
+#include "BDSDebug.hh"
+#include "globals.hh"
 
 BDSBeamPipeType BDS::DetermineBeamPipeType(G4String apertureType)
 {
-  if( !strcmp(apertureType , "circular") )
-    {return BDSBeamPipeType::circular;}
-  else if ( !strcmp(apertureType, "elliptical") )
-    {return BDSBeamPipeType::elliptical;}
-  else if ( !strcmp(apertureType, "rectanglular") )
-    {return BDSBeamPipeType::rectangular;}
-
+  G4cout << apertureType << G4endl;
+  if( apertureType.compare("circular") == 0 )
+    {
+#ifdef BDSDEBUG
+      G4cout << __METHOD_NAME__ << " -> circular beampipe" << G4endl;
+#endif
+      return BDSBeamPipeType::circular;
+    }
+  else if ( apertureType.compare("elliptical") == 0 )
+    {
+#ifdef BDSDEBUG
+      G4cout << __METHOD_NAME__ << " -> elliptical beampipe" << G4endl;
+#endif
+      return BDSBeamPipeType::elliptical;
+    }
+  else if ( apertureType.compare("rectangular") == 0 )
+    {
+#ifdef BDSDEBUG
+      G4cout << __METHOD_NAME__ << " -> rectangular beampipe" << G4endl;
+#endif
+      return BDSBeamPipeType::rectangular;
+    }
   else
-    {return BDSBeamPipeType::circular;} // default case
+    {
+#ifdef BDSDEBUG
+      G4cout << __METHOD_NAME__ << " -> Unknown beampipe type " << apertureType  << " making a circular beampipe" << G4endl;
+#endif
+      return BDSBeamPipeType::circular;
+    } // default case
 }
 
 BDSBeamPipeType BDS::DetermineBeamPipeType(Element& element)
