0a4beabcacbe86a1b64586da4f5db60f1b804eb7
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2015-03-16 23:34:52 +0000

    add info class for magnet outer bodies to reduce constructors and allow future extension

---
+++ include/BDSMagnetOuterInfo.hh
@@ -0,0 +1,31 @@
+#ifndef BDSMAGNETOUTERINFO
+#define BDSMAGNETOUTERINFO
+
+#include "BDSMagnetGeometryType.hh"
+#include "globals.hh"         // geant4 types / globals
+#include "G4Material.hh"
+
+/**
+ * @brief Holder struct of all information required to
+ * create the outer geometry of a magnet. This reduces 
+ * the number of argument to all magnet constructors 
+ * plus aggregates common tasks in the component factory.
+ * 
+ * @author Laurie Nevay <laurie.nevay@rhul.ac.uk>
+ */
+
+struct BDSMagnetOuterInfo {
+  /// default constructor
+  BDSMagnetOuterInfo();
+
+  /// extra constructor to assign all members at once
+  BDSMagnetOuterInfo(BDSMagnetGeometryType geometryTypeIn,
+		     G4double              outerDiameterIn,
+		     G4Material*           outerMaterialIn);
+		  
+  BDSMagnetGeometryType geometryType;
+  G4double        outerDiameter;
+  G4Material*     outerMaterial;
+};
+
+#endif
