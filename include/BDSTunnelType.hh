394d5d0897312d307ce0cfdef768cf086b40d2f2
Jochem Snuverink <Jochem.Snuverink@rhul.ac.uk>
2015-05-07 12:02:09 +0100

    remove dependency on globalconstants from tunneltype class

--- include/BDSTunnelType.hh
+++ include/BDSTunnelType.hh
@@ -18,34 +18,35 @@ struct tunneltypes_def {
 typedef BDSTypeSafeEnum<tunneltypes_def,int> BDSTunnelType;
 
 namespace BDS {
-  BDSTunnelType DetermineTunnelType(G4String tunnelType, G4bool globalCheck=false);
+  /// function to determine the enum type of the tunnel (case-insensitive)
+  BDSTunnelType DetermineTunnelType(G4String tunnelType);
 
   /*
   /// function to check relevant aperture values are set.  This is really a dispatch function
   /// for other aperture specific methods below
   void CheckTunnelInfo(BDSTunnelType tunnelTypeIn,
-		       G4double&     tunnelOffsetX,
-		       G4double&     tunnelOffsetY,
-		       G4bool&       tunnelFloor,
-		       G4double&     tunnelFloorOffset,
-		       G4double&     tunnel1,
-		       G4double&     tunnel2);
+		       G4double     tunnelOffsetX,
+		       G4double     tunnelOffsetY,
+		       G4bool       tunnelFloor,
+		       G4double     tunnelFloorOffset,
+		       G4double     tunnel1,
+		       G4double     tunnel2);
 
   /// aperture info check for circular tunnel
-  void InfoOKForCircular(G4double& tunnelOffsetX, G4double& tunnelOffsetY, G4bool& tunnelFloor, G4double& tunnelFloorOffset,
-			 G4double& tunnel1, G4double& tunnel2);
+  void InfoOKForCircular(G4double tunnelOffsetX, G4double tunnelOffsetY, G4bool tunnelFloor, G4double tunnelFloorOffset,
+			 G4double tunnel1, G4double tunnel2);
 
   /// aperture info check for elliptical tunnel
-  void InfoOKForElliptical(G4double& tunnelOffsetX, G4double& tunnelOffsetY, G4bool& tunnelFloor, G4double& tunnelFloorOffset,
-			   G4double& tunnel1, G4double& tunnel2);
+  void InfoOKForElliptical(G4double tunnelOffsetX, G4double tunnelOffsetY, G4bool tunnelFloor, G4double tunnelFloorOffset,
+			   G4double tunnel1, G4double tunnel2);
 
   /// aperture info check for rectangular tunnel
-  void InfoOKForRectangular(G4double& tunnelOffsetX, G4double& tunnelOffsetY, G4bool& tunnelFloor, G4double& tunnelFloorOffset,
-			    G4double& tunnel1, G4double& tunnel2);
+  void InfoOKForRectangular(G4double tunnelOffsetX, G4double tunnelOffsetY, G4bool tunnelFloor, G4double tunnelFloorOffset,
+			    G4double tunnel1, G4double tunnel2);
 
   /// aperture info check for rectangular tunnel
-  void InfoOKForSquare(G4double& tunnelOffsetX, G4double& tunnelOffsetY, G4bool& tunnelFloor, G4double& tunnelFloorOffset,
-		       G4double& tunnel1, G4double& tunnel2);
+  void InfoOKForSquare(G4double tunnelOffsetX, G4double tunnelOffsetY, G4bool tunnelFloor, G4double tunnelFloorOffset,
+		       G4double tunnel1, G4double tunnel2);
   */
 }
 #endif
