973bcfe9a261572b633cb7a7c45d6cc39efe60b5
Jochem Snuverink <Jochem.Snuverink@rhul.ac.uk>
2015-05-14 14:43:00 +0200

    first version of tunnel class for parser

---
+++ parser/tunnel.cc
@@ -0,0 +1,6 @@
+#include "tunnel.h"
+
+Tunnel::Tunnel():radius(0.0),offsetX(0.0),offsetY(0.0),tunnelThickness(0.0),tunnelSoilThickness(0.0),tunnelFloorOffset(0.0)
+{
+  // better default values?
+}
---
+++ parser/tunnel.h
@@ -0,0 +1,39 @@
+#ifndef __TUNNEL_H
+#define __TUNNEL_H
+
+#include <string>
+
+/**
+ * @brief Tunnel class for parser
+ * 
+ * @author Jochem Snuverink <Jochem.Snuverink@rhul.ac.uk> 
+ */
+
+struct Tunnel {
+  /// geometry type
+  std::string type;
+
+  /// radius
+  double   radius;
+  /// offset x and y
+  double   offsetX, offsetY;
+  /// tunnel geometry parameters
+  double   tunnelThickness;
+  double   tunnelSoilThickness;
+  double   tunnelFloorOffset;
+  
+  /// material
+  std::string material;
+  //std::string tunnelCavityMaterial;
+  /// soil
+  std::string soilMaterial;
+
+  /// start and end element by name
+  std::string startElement, endElement;
+  
+  /// contructor
+  Tunnel();
+  
+};
+
+#endif
