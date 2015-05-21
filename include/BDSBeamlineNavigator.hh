51c91a9aa244eef26e13cc84d80c1e154a4e2db1
Jochem Snuverink <Jochem.Snuverink@rhul.ac.uk>
2015-05-21 17:27:19 +0100

    clean navigator

--- include/BDSBeamlineNavigator.hh
+++ include/BDSBeamlineNavigator.hh
@@ -15,22 +15,23 @@ public:
   ~BDSBeamlineNavigator();
   
   void addComponent(BDSAcceleratorComponent* var);
-  void print();
+  void print()const;
 
-  G4RotationMatrix* rotation();
-  G4RotationMatrix* rotationGlobal();
-  G4ThreeVector* position();
-  G4double       positionS();
-  G4ThreeVector* positionStart();
-  G4ThreeVector* positionEnd();
-  G4ThreeVector* positionFromCurrentCenter();
-  G4ThreeVector* GetLastPosition();
-  G4ThreeVector* GetFirstPosition();
-  G4ThreeVector GetMaximumExtentPositive();
-  G4ThreeVector GetMaximumExtentNegative();
+  G4RotationMatrix* rotation()const;
+  G4RotationMatrix* rotationGlobal()const;
+  G4ThreeVector* position()const;
+  G4double       positionS()const;
+  G4ThreeVector* positionStart()const;
+  G4ThreeVector* positionEnd()const;
+  G4ThreeVector* positionFromCurrentCenter()const;
+  G4ThreeVector* GetLastPosition()const;
+  G4ThreeVector* GetFirstPosition()const;
+  G4ThreeVector GetMaximumExtentPositive()const;
+  G4ThreeVector GetMaximumExtentNegative()const;
 
-  G4double s_total();
+  G4double s_total()const;
 
+  /// for iterators. should be separate class perhaps
   void first();
   bool isDone();
   void next();
@@ -40,37 +41,37 @@ private:
   BDSBeamlineNavigator& operator=(const BDSBeamlineNavigator&);
   BDSBeamlineNavigator(BDSBeamlineNavigator&);
 
-  G4ThreeVector* _localX; 
-  G4ThreeVector* _localY; 
-  G4ThreeVector* _localZ; 
-  G4ThreeVector* _position;
-  G4ThreeVector* _positionStart;
-  G4ThreeVector* _positionEnd;
-  G4ThreeVector* _positionFromCurrentCenter;
-  G4ThreeVector* _zHalfAngle;
-  G4RotationMatrix* _rotationLocal;
+  G4ThreeVector* _localX; ///> current local x direction
+  G4ThreeVector* _localY; ///> current local y direction
+  G4ThreeVector* _localZ; ///> current local z direction
+
   G4RotationMatrix* _rotationGlobal;
   G4RotationMatrix* _rotation;
+  ///@{ maximum and minimum position, for world dimensions
   G4ThreeVector _maximumExtentPositive;
   G4ThreeVector _maximumExtentNegative;
-
+  ///@}
+  
   std::list<G4RotationMatrix*> _rotationList;
   std::list<G4RotationMatrix*> _rotationGlobalList;
-  std::list<G4ThreeVector*> _positionList;
-  std::list<G4ThreeVector*> _positionStartList;
-  std::list<G4ThreeVector*> _positionEndList;
+  std::list<G4ThreeVector*> _positionList; ///> Centre global positions
+  std::list<G4ThreeVector*> _positionStartList; ///> Start positions
+  std::list<G4ThreeVector*> _positionEndList; ///> End positions
+  /// The position of the beam line component from the centre of the CURRENT beam line
   std::list<G4ThreeVector*> _positionFromCurrentCenterList;
-  std::list <G4double> _positionSList; //Position along the curvilinear coordinate "s"
+  std::list <G4double> _positionSList; ///> Position along the curvilinear coordinate "s"
 
+  ///@{ iterator for corresponding list
   std::list<G4RotationMatrix*>::const_iterator  _iterRotation;
   std::list<G4RotationMatrix*>::const_iterator  _iterRotationGlobal;
   std::list<G4ThreeVector*>::const_iterator  _iterPosition;
   std::list<G4ThreeVector*>::const_iterator  _iterPositionStart;
   std::list<G4ThreeVector*>::const_iterator  _iterPositionEnd;
   std::list<G4ThreeVector*>::const_iterator  _iterPositionFromCurrentCenter;
-  std::list<G4double>::const_iterator _iterPositionS; //Position along the curvilinear coordinate "s"
-
-  G4double _s_total;
+  std::list<G4double>::const_iterator _iterPositionS;
+  ///@}
+  
+  G4double _s_total; ///> beamline length
 };
 
 #endif
