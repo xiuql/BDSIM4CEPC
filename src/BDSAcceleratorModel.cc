2c8609d379fcc19411f2bf07af72f07b901cc25c
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2015-06-01 00:14:41 +0100

    provide place to keep new beamline instance as it's not a singleton any more

--- src/BDSAcceleratorModel.cc
+++ src/BDSAcceleratorModel.cc
@@ -13,5 +13,8 @@ BDSAcceleratorModel* BDSAcceleratorModel::Instance()
 
 
 BDSAcceleratorModel::BDSAcceleratorModel():
-  readOutWorldPV(NULL),readOutWorldLV(NULL)
+  worldPV(NULL),
+  readOutWorldPV(NULL),
+  readOutWorldLV(NULL),
+  flatBeamline(NULL)
 {;}
