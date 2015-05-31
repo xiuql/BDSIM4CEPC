3b5a180cb42ad24eaea5100d0c3f07abe9694abb
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2015-06-01 00:14:00 +0100

    rewrite BDSBeamline - null implementation just now as purely defining interface. All positions and rotations are not calculated yet and are null

--- include/BDSBeamline.hh
+++ include/BDSBeamline.hh
@@ -1,69 +1,156 @@
-#ifndef __BDSBEAMLINE_H
-#define __BDSBEAMLINE_H
+#ifndef BDSBEAMLINE_H
+#define BDSBEAMLINE_H
 
-#include <list>
+#include "globals.hh" // geant4 globals / types
 
-#include "globals.hh"
+#include "BDSBeamlineElement.hh"
 
-#include "BDSAcceleratorComponent.hh"
-
-class BDSBeamlineNavigator;
+#include <iterator>
+#include <ostream>
+#include <utility>    //for std::pair
+#include <vector>
 
 /**
- * @brief a class that holds the beamline.
+ * @brief A vector of BDSBeamlineElement instances - a beamline.
  * 
- * singleton pattern, and owns the memory of its components
+ * This will calculate and construct a beamline as BDSAcceleratorComponents
+ * are added in sequence - ie it calculates their placement positions and 
+ * rotations with respect to the start of the beamline as well as their s
+ * position in curvilinear coordinates.
+ * 
+ * Note, this is note a singleton as geometry hierarchy can be introduced
+ * by placing beamline components inside parent volumes and therefore creating
+ * a new beamline of parents.
+ * 
+ * Note, the beamline will only calculate coordinates relative to its start
+ * being 0,0,0. For placement from a different position, this should be applied
+ * elsewhere when the placement is being performed and not in the calculated 
+ * coordinates here.
  */
+
+/// Forward declaration for iterator
+class BDSBeamline;
+
+/// Iterator for beamline
+typedef std::vector<BDSBeamlineElement*>::const_iterator BDSBeamlineIterator;
 
 class BDSBeamline{
 public:
-
-  static BDSBeamline* Instance();
-  ~BDSBeamline();
-  
-  void addComponent(BDSAcceleratorComponent* var);
-  void print();
-  BDSAcceleratorComponent* currentItem();
-  BDSAcceleratorComponent* lastItem();
-  BDSAcceleratorComponent* firstItem();
-  G4bool isLast();
-  void first();
-  bool isDone();
-  void next();
-
-  G4double s_total();
-
-  G4RotationMatrix* rotation();
-  G4RotationMatrix* rotationGlobal();
-  G4ThreeVector* position();
-  G4double       positionS();
-  G4ThreeVector* positionStart();
-  G4ThreeVector* positionEnd();
-  G4ThreeVector* positionFromCurrentCenter();
-  G4ThreeVector* GetLastPosition();
-  G4ThreeVector* GetFirstPosition();
-  G4ThreeVector  GetMaximumExtentPositive();
-  G4ThreeVector  GetMaximumExtentNegative();
-
-  G4int size();
-
-private: 
   BDSBeamline();
+  ~BDSBeamline();
 
-  static BDSBeamline* _instance;
-  std::list <BDSAcceleratorComponent*> _componentList;
+  /// Add a component and calculate its position and rotation with respect
+  /// to the beginning of the beamline
+  void AddComponent(BDSAcceleratorComponent* component);
 
-  std::list<BDSAcceleratorComponent*>::const_iterator _iterComponent;
-  std::list<BDSAcceleratorComponent*>::const_iterator _iterLastComponent;
+  /// Iterate over the beamline and print out the name, position, rotation
+  /// and s position of each beamline element
+  void PrintAllComponents(std::ostream& out) const;
 
-  /// navigator object
-  BDSBeamlineNavigator* _navigator;
+  BDSBeamlineElement* GetFirstItem();             ///< Return a reference to the first element
+  BDSBeamlineElement* GetLastItem();              ///< Return a reference to the last element
 
-  void setRefTransform(BDSAcceleratorComponent* var);
+  /// Get the total length of the beamline - the sum of the chord length of each element
+  inline G4double     GetTotalChordLength() const;
 
+  /// Get the total ARC length fo the beamline - ie the maximum s position
+  inline G4double     GetTotalArcLength() const;
+
+  /// Get the number of elements
+  std::vector<BDSBeamlineElement*>::size_type size() const;
+  
+  G4ThreeVector       GetMaximumExtentPositive(); ///< Get the maximum positive extent in all dimensions  
+  G4ThreeVector       GetMaximumExtentNegative(); ///< Get the maximum negative extent in all dimensions
+
+  // Accessors in a similar style to std::vector
+  /// Return a reference to the first element
+  inline BDSBeamlineElement* front() const;
+  /// Return a reference to the last element
+  inline BDSBeamlineElement* back()  const;
+  /// Return iterator to the beginning
+  inline std::vector<BDSBeamlineElement*>::iterator begin();
+  /// Return iterator to the end
+  inline std::vector<BDSBeamlineElement*>::iterator end();
+  /// Return reverse iterator to the reverse beginning
+  inline std::vector<BDSBeamlineElement*>::reverse_iterator rbegin();
+  /// Return reverse iterator to the reverse end
+  inline std::vector<BDSBeamlineElement*>::reverse_iterator rend();
+  /// Return iterator to the beginning
+  inline std::vector<BDSBeamlineElement*>::const_iterator cbegin() const;
+  /// Return iterator to the end
+  inline std::vector<BDSBeamlineElement*>::const_iterator cend()   const;
+  /// Return reverse iterator to the reverse beginning
+  inline std::vector<BDSBeamlineElement*>::const_reverse_iterator crbegin() const;
+  /// Return reverse iterator to the reverse end
+  inline std::vector<BDSBeamlineElement*>::const_reverse_iterator crend()   const;
+
+  /// output stream
+  friend std::ostream& operator<< (std::ostream &out, BDSBeamline const &beamline);
+  
+private: 
+  std::vector<BDSBeamlineElement*> beamline; ///< Beamline vector - the data
+
+  G4double totalChordLength;
+  G4double totalArcLength;
+
+  std::pair<G4double, G4double> extentX;     ///< +ve and -ve extent in X
+  std::pair<G4double, G4double> extentY;     ///< +ve and -ve extent in Y
+  std::pair<G4double, G4double> extentZ;     ///< +ve and -ve extent in Z
+  
   /// assignment and copy constructor not implemented nor used
   BDSBeamline& operator=(const BDSBeamline&);
   BDSBeamline(BDSBeamline&);
 };
+
+inline BDSBeamlineElement* BDSBeamline::GetFirstItem()
+{return front();}
+
+inline BDSBeamlineElement* BDSBeamline::GetLastItem()
+{return back();}
+
+inline G4double BDSBeamline::GetTotalChordLength() const
+{return totalChordLength;}
+
+inline G4double BDSBeamline::GetTotalArcLength() const
+{return totalArcLength;}
+
+inline std::vector<BDSBeamlineElement*>::size_type BDSBeamline::size() const
+{return beamline.size();}
+
+inline G4ThreeVector BDSBeamline::GetMaximumExtentPositive()
+{return G4ThreeVector(extentX.second, extentY.second, extentZ.second);}
+
+inline G4ThreeVector BDSBeamline::GetMaximumExtentNegative()
+{return G4ThreeVector(extentX.first, extentY.first, extentZ.first);}
+
+inline BDSBeamlineElement* BDSBeamline::front() const
+{return beamline.front();}
+
+inline BDSBeamlineElement* BDSBeamline::back() const
+{return beamline.back();}
+
+inline std::vector<BDSBeamlineElement*>::iterator BDSBeamline::begin()
+{return beamline.begin();}
+
+inline std::vector<BDSBeamlineElement*>::iterator BDSBeamline::end()
+{return beamline.end();}
+
+inline std::vector<BDSBeamlineElement*>::reverse_iterator BDSBeamline::rbegin()
+{return beamline.rbegin();}
+
+inline std::vector<BDSBeamlineElement*>::reverse_iterator BDSBeamline::rend()
+{return beamline.rend();}
+
+inline std::vector<BDSBeamlineElement*>::const_iterator BDSBeamline::cbegin() const
+{return beamline.begin();}
+
+inline std::vector<BDSBeamlineElement*>::const_iterator BDSBeamline::cend() const
+{return beamline.end();}
+
+inline std::vector<BDSBeamlineElement*>::const_reverse_iterator BDSBeamline::crbegin() const
+{return beamline.rbegin();}
+
+inline std::vector<BDSBeamlineElement*>::const_reverse_iterator BDSBeamline::crend() const
+{return beamline.rend();}
 
 #endif
--- src/BDSBeamline.cc
+++ src/BDSBeamline.cc
@@ -1,148 +1,77 @@
+#include "globals.hh" // geant4 globals / types
+#include "G4RotationMatrix.hh"
+#include "G4ThreeVector.hh"
+
 #include "BDSDebug.hh"
+#include "BDSAcceleratorComponent.hh"
 #include "BDSBeamline.hh"
-#include "G4AffineTransform.hh"
-#include "BDSBeamlineNavigator.hh"
+#include "BDSBeamlineElement.hh"
 
-BDSBeamline* BDSBeamline::_instance = 0;
+#include <iterator>
+#include <ostream>
+#include <utility>  // for std::pair
+#include <vector>
 
-BDSBeamline* BDSBeamline::Instance(){
-  if(_instance==0) {
-    _instance = new BDSBeamline();
-  }
-  return _instance;
+BDSBeamline::BDSBeamline()
+{;}
+
+BDSBeamline::~BDSBeamline()
+{
+  std::vector<BDSBeamlineElement*>::iterator it = begin();
+  for (; it != end(); ++it)
+    {delete (*it);}
 }
 
-BDSBeamline::BDSBeamline(){
-  _navigator = new BDSBeamlineNavigator();
+void BDSBeamline::PrintAllComponents(std::ostream& out) const
+{
+  std::vector<BDSBeamlineElement*>::const_iterator it = cbegin();
+  for (; it != cend(); ++it)
+    {out << *(it);}
 }
 
-BDSBeamline::~BDSBeamline(){
-  for(first(); !isDone(); next()){
-    delete *_iterComponent;
-  }
-  _componentList.clear();
-  delete _navigator;
-  _instance = 0;
+std::ostream& operator<< (std::ostream& out, BDSBeamline const &beamline)
+{
+  out << "BDSBeamline with " << beamline.size() << " elements"<< G4endl
+      << "Elements are: " << G4endl;
+  beamline.PrintAllComponents(out);
+
+  return out;
 }
 
-void BDSBeamline::addComponent(BDSAcceleratorComponent* var){
-  //Add component to the beamline
+void BDSBeamline::AddComponent(BDSAcceleratorComponent* component)
+{
 #ifdef BDSDEBUG
   G4cout << __METHOD_NAME__ << " adding component" << G4endl;
 #endif
+  // test if component has finite length (different action in that case)
 
-  _componentList.push_back(var);
-  _iterComponent=_componentList.end();
+  // calculate the placement position
+  G4ThreeVector positionStart  = G4ThreeVector(0,0,0);
+  G4ThreeVector positionMiddle = G4ThreeVector(0,0,0);
+  G4ThreeVector positionEnd    = G4ThreeVector(0,0,0);
 
-  //Do the navigation
-  _navigator->addComponent(var);
+  // calculate the placement rotation
+  G4RotationMatrix* rotationStart  = new G4RotationMatrix();
+  G4RotationMatrix* rotationMiddle = new G4RotationMatrix();
+  G4RotationMatrix* rotationEnd    = new G4RotationMatrix();
 
-#ifdef BDSDEBUG
-  G4cout << __METHOD_NAME__ << " last item is " << lastItem()->GetName() << G4endl;
-#endif
-  //Update the reference transform
-  setRefTransform(var);
-}
+  // calculate the s position
+  G4double sPositionStart  = 0;
+  G4double sPositionMiddle = 0;
+  G4double sPositionEnd    = 0;
 
-void BDSBeamline::setRefTransform(BDSAcceleratorComponent* var){
-  if(BDSGlobalConstants::Instance()->GetRefVolume()==var->GetName() && 
-     BDSGlobalConstants::Instance()->GetRefCopyNo()==var->GetCopyNumber()){
-#ifdef BDSDEBUG
-    G4cout << __METHOD_NAME__ << "setting new transform" <<G4endl;
-#endif
-    G4AffineTransform tf(rotationGlobal(),*positionStart());
-    BDSGlobalConstants::Instance()->SetRefTransform(tf);
-  }
-}
-
-void BDSBeamline::print(){
-  for(first(); !isDone(); next()){
-    G4cout << (*_iterComponent)->GetName() << G4endl;
-    _navigator->print();
-  }
-}
-
-BDSAcceleratorComponent* BDSBeamline::currentItem(){
-  return *_iterComponent;
-}
-
-BDSAcceleratorComponent* BDSBeamline::lastItem(){
-  if (_componentList.empty()) return NULL;
-  return _componentList.back();
-}
-
-BDSAcceleratorComponent* BDSBeamline::firstItem(){
-  return _componentList.front();
-}
-
-G4bool BDSBeamline::isLast(){
-  return (*_iterComponent == _componentList.back());
-}
-
-G4int BDSBeamline::size(){
-  return _componentList.size();
-}
-
-void BDSBeamline::first(){
-  _iterComponent=_componentList.begin();
-  //Navigation
-  _navigator->first();
-}
-
-bool BDSBeamline::isDone(){
-  return (_iterComponent==_componentList.end());
-}
-
-void BDSBeamline::next(){
-  _iterComponent++;
-  //Navigation
-  _navigator->next();
-}
-
-G4RotationMatrix* BDSBeamline::rotation(){
-  return _navigator->rotation();
-}
-
-G4RotationMatrix* BDSBeamline::rotationGlobal(){
-  return _navigator->rotationGlobal();
-}
-
-G4ThreeVector* BDSBeamline::position(){
-  return _navigator->position();
-}
-
-G4ThreeVector* BDSBeamline::positionStart(){
-  return _navigator->positionStart();
-}
-
-G4ThreeVector* BDSBeamline::positionEnd(){
-  return _navigator->positionEnd();
-}
-
-G4ThreeVector* BDSBeamline::positionFromCurrentCenter(){
-  return _navigator->positionFromCurrentCenter();
-}
-
-G4double BDSBeamline::positionS(){
-  return _navigator->positionS();
-}
-
-G4double BDSBeamline::s_total(){
-  return _navigator->s_total();
-}
-
-G4ThreeVector* BDSBeamline::GetLastPosition(){
-  return _navigator->GetLastPosition();
-}
-
-G4ThreeVector* BDSBeamline::GetFirstPosition(){
-  return _navigator->GetFirstPosition();
-}
-
-G4ThreeVector BDSBeamline::GetMaximumExtentPositive(){
-  return _navigator->GetMaximumExtentPositive();
-}
-
-G4ThreeVector BDSBeamline::GetMaximumExtentNegative(){
-  return _navigator->GetMaximumExtentNegative();
+  // construct beamline element
+  BDSBeamlineElement* element = new BDSBeamlineElement(component,
+						       positionStart,
+						       positionMiddle,
+						       positionEnd,
+						       rotationStart,
+						       rotationMiddle,
+						       rotationEnd,
+						       sPositionStart,
+						       sPositionMiddle,
+						       sPositionEnd);
+  
+  // append it to the beam line
+  beamline.push_back(element);
 }
