4d52395fec53257d0dfbb8ce30d8905f86052105
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2015-06-02 22:33:09 +0100

    utility function to determine whether a value is finite - should be use throughout bdsim

--- include/BDSUtilities.hh
+++ include/BDSUtilities.hh
@@ -13,20 +13,20 @@
  */
 
 namespace BDS {
-  /// logical not for isalpha predictator as needed for string manipulations
+  /// Logical not for isalpha predictator as needed for string manipulations
   struct non_alpha
   {
     G4bool operator()(char c);
   };
 
-  // remove white space and special characters in the name
+  /// Remove white space and special characters in the name
   G4String PrepareSafeName(G4String name);
 
-  /// calculate the +- 1 orientation multiplier for absolute angles
+  /// Calculate the +- 1 orientation multiplier for absolute angles
   /// seems trivial, but used in a lot of places so put in one place
   G4int    CalculateOrientation(G4double angle);
 
-  /// returns path from which BDSIM is executed
+  /// Returns path from which BDSIM is executed
   /// supports linux/unix and mac OS
   std::string GetBDSIMExecPath();
 
@@ -35,6 +35,9 @@ namespace BDS {
       Main goal is to close output stream / files.
   */
   void HandleAborts(int signal_number);
+
+  /// Determine whether a parameter is finite
+  G4bool IsFinite(const G4double& variable);
 }
 
 
