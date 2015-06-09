92faf0c86b139ae23265bda702f2b98944a428c6
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2015-06-09 22:24:17 +0100

    BDSTransform3D reimplementation to match changes to BDSAcceleratorComponent

--- include/BDSTransform3D.hh
+++ include/BDSTransform3D.hh
@@ -1,27 +1,62 @@
-/* BDSIM code.    Version 1.0
-  transform3d
-*/
-
-#ifndef __BDSTransform3D_h
-#define __BDSTransform3D_h
+#ifndef BDSTRANSFORM3D_H
+#define BDSTRANSFORM3D_H
 
 #include "globals.hh"
 #include "BDSAcceleratorComponent.hh"
 
-class BDSTransform3D : public BDSAcceleratorComponent
+class BDSTransform3D: public BDSAcceleratorComponent
 {
 public:
-
   BDSTransform3D(G4String aName, G4double x,
 		 G4double y, G4double z, G4double phi,
 		 G4double theta, G4double psi);
 
   ~BDSTransform3D();
+
+  ///@{ Access the change in reference coordinates this component should induce
+  inline G4double GetDX() const;
+  inline G4double GetDY() const;
+  inline G4double GetDZ() const;
+  ///@}
   
+  ///@{ Access the change in Euler angle this component should induce
+  inline G4double GetDTheta() const;
+  inline G4double GetDPsi()   const;
+  inline G4double GetDPhi()   const;
+  ///@}
   
 private:
-  
+  /// Contractual function overload from virtual base class
+  /// BDSAcceleratorComponent. In this case has null implementation
+  /// as the transform only inherits BDSAcceleratorComponent so that
+  /// is compatable with the beamline
   virtual void BuildContainerLogicalVolume(){;};
+
+  G4double dx;
+  G4double dy;
+  G4double dz;
+  G4double dTheta;
+  G4double dPsi;
+  G4double dPhi;
 };
+
+inline G4double BDSTransform3D::GetDX() const
+{return dx;}
+
+inline G4double BDSTransform3D::GetDY() const
+{return dy;}
+
+inline G4double BDSTransform3D::GetDZ() const
+{return dz;}
+
+inline G4double BDSTransform3D::GetDTheta() const
+{return dTheta;}
+
+inline G4double BDSTransform3D::GetDPsi() const
+{return dPsi;}
+
+inline G4double BDSTransform3D::GetDPhi() const
+{return dPhi;}
+
 
 #endif
--- src/BDSTransform3D.cc
+++ src/BDSTransform3D.cc
@@ -1,23 +1,16 @@
 #include "BDSTransform3D.hh"
 
-BDSTransform3D::BDSTransform3D(G4String aName,
-			       G4double x,
-			       G4double y,
-			       G4double z,
-			       G4double phi,
-			       G4double theta,
-			       G4double psi):
-  BDSAcceleratorComponent(name,0,0,"transform3d")
-{
-  /*
-  SetTheta(theta);
-  SetPsi(psi);
-  SetPhi(phi);
-  */
-
-  // well this is broken for now :(
-}
+BDSTransform3D::BDSTransform3D(G4String name,
+			       G4double dxIn,
+			       G4double dyIn,
+			       G4double dzIn,
+			       G4double dPhiIn,
+			       G4double dThetaIn,
+			       G4double dPsiIn):
+  BDSAcceleratorComponent(name,0,0,"transform3d"),
+  dx(dxIn), dy(dyIn), dz(dzIn),
+  dTheta(dThetaIn), dPsi(dPsiIn), dPhi(dPhiIn)
+{;}
 
 BDSTransform3D::~BDSTransform3D()
-{
-}
+{;}
