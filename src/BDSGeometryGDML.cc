fbf7b4b433f45645f2167ee77e65b22e67a759f4
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2015-06-26 13:34:12 +0100

    Fixed visualisation of GDML

--- src/BDSGeometryGDML.cc
+++ src/BDSGeometryGDML.cc
@@ -2,6 +2,7 @@
 #include "BDSGlobalConstants.hh"
 #include "BDSGeometryGDML.hh"
 #include "BDSMaterials.hh"
+#include "BDSMagnetColours.hh"
 #include "G4Colour.hh"
 #include "G4GDMLParser.hh"
 #include "G4LogicalVolume.hh"
@@ -25,17 +26,17 @@ void BDSGeometryGDML::Construct(G4LogicalVolume *marker){
   
   G4LogicalVolume* topvol = parser->GetWorldVolume()->GetLogicalVolume();
 
-  G4VisAttributes* VisAtt = new G4VisAttributes(G4Colour(0.0, 1.0, 0.0));
+  G4VisAttributes* VisAtt = new G4VisAttributes(*BDSMagnetColours::Instance()->GetMagnetColour("gdml"));
   VisAtt->SetVisibility(false);
 
-  G4VisAttributes* VisAtt2 = new G4VisAttributes(G4Colour(0.0, 1.0, 0.0));
-  VisAtt2->SetVisibility(false);
-  
+  G4VisAttributes* VisAtt2 = new G4VisAttributes(*BDSMagnetColours::Instance()->GetMagnetColour("gdml"));
+  VisAtt2->SetVisibility(true);
+
   for (int i=0; i<topvol->GetNoDaughters(); i++){
-    topvol->GetDaughter(i)->GetLogicalVolume()->SetVisAttributes(VisAtt);
+    topvol->GetDaughter(i)->GetLogicalVolume()->SetVisAttributes(VisAtt2);
   }
   
-  topvol->SetVisAttributes(VisAtt2);
+  topvol->SetVisAttributes(VisAtt);
   
   new G4PVPlacement(NULL,
                     G4ThreeVector(0.,0.,0.),
