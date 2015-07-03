8c66469bfe09aab2db2dde01c6211a861b6b7c88
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2015-07-03 22:44:08 +0100

    fix priority of output instantiation; fix return type; fix duplicate return of outputs resulting in 'combined' output producing ascii + root + ascii.

--- src/BDSOutputFactory.cc
+++ src/BDSOutputFactory.cc
@@ -7,26 +7,39 @@
 #include "BDSOutputROOT.hh"
 #include "BDSOutputVector.hh"
 
-BDSOutputBase* BDSOutputFactory::createOutput(BDSOutputFormat format) {
-  if (format == BDSOutputFormat::_NONE) {
-    return new BDSOutputNone();
-  }
-  else if (format == BDSOutputFormat::_ASCII) {
-    return new BDSOutputASCII();
-  }
-  else if (format == BDSOutputFormat::_ROOT) {
+#include "BDSDebug.hh"
+
+BDSOutputBase* BDSOutputFactory::createOutput(BDSOutputFormat format)
+{
+#ifdef BDSDEBUG
+  G4cout << __METHOD_NAME__ << "output format = " << format << G4endl;
+#endif
+  if (format == BDSOutputFormat::_NONE)
+    {return new BDSOutputNone();}
+  
+  if (format == BDSOutputFormat::_COMBINED)
+    {
+      BDSOutputVector* combinedOutput = new BDSOutputVector();
+      combinedOutput->Add(new BDSOutputASCII());
+#ifdef USE_ROOT
+      combinedOutput->Add(new BDSOutputROOT());
+#endif
+      return combinedOutput;
+    }
+  
+  else if (format == BDSOutputFormat::_ASCII)
+    {return new BDSOutputASCII();}
+  
+  else if (format == BDSOutputFormat::_ROOT)
+    {
 #ifdef USE_ROOT
     return new BDSOutputROOT();
+#else
+    G4cout << __METHOD_NAME__ << "warning: root output selected but BDSIM not built with root support" << G4endl;
+    G4cout << __METHOD_NAME__ << "using ASCII by default" << G4endl;
+    return new BDSOutputASCII();
 #endif
-  }
-  else if (format == BDSOutputFormat::_COMBINED) {
-    BDSOutputVector* combinedOutput = new BDSOutputVector();
-    combinedOutput->Add(new BDSOutputASCII());
-#ifdef USE_ROOT
-    combinedOutput->Add(new BDSOutputROOT());
-#endif
-    bdsOutput = combinedOutput;
-  }
-  // should not happen, but to prevent compiler warning
-  return new BDSOutputASCII();
+    }
+  else
+    {return NULL;}
 }
