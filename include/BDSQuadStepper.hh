ba6017d1d2c1ba56ea1abc95c713161ab869f34e
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2015-07-22 23:46:13 +0100

    improved doxygen documentation; initialise navigator only once

--- include/BDSQuadStepper.hh
+++ include/BDSQuadStepper.hh
@@ -28,8 +28,8 @@ public:  // with description
   G4double DistChord()   const;
   // Estimate maximum distance of curved solution and chord ... 
  
-  void SetBGrad(G4double aBGrad);
-  G4double GetBGrad();
+  void     SetBGrad(G4double aBGrad);
+  G4double GetBGrad() const;
 
   void StepperName();
 
@@ -40,30 +40,44 @@ public: // without description
 protected:
   //  --- Methods used to implement all the derived classes -----
 
-  void AdvanceHelix( const G4double  yIn[],
-		     G4ThreeVector Bfld,
-		     G4double  h,
-		     G4double  yQuad[]);    // output 
+  void AdvanceHelix(const G4double  yIn[],
+		    G4ThreeVector Bfld,
+		    G4double  h,
+		    G4double  yQuad[]);    // output 
   // A first order Step along a quad inside the field.
 
 private:
+  /// Keep a local navigator to work out positions and transforms at various points.
+  /// If we use the general navigator, then this actually changes the position of the
+  /// particle being tracked.
   G4Navigator* QuadNavigator;
+
+  /// Charge
   G4Mag_EqRhs* fPtrMagEqOfMot;
 
+  /// B Field Gradient
   G4double itsBGrad;
 
+  /// Data stored in order to find the chord.
   G4ThreeVector yInitial, yMidPoint, yFinal;
-  // Data stored in order to find the chord.
 
+  /// Local variable used to passs information to DistChord() which gives the distance
+  /// from the chord calculated by the in and out points and the true curved path the
+  /// particle would take in the field.
   G4double itsDist;
 
+  /// Record whether the class has been used or not. Use to get the
+  /// world volume for a local navigator object on first use. We have
+  /// to do this because the stepper objects are typically created before
+  /// the world is constructed and registered. However, we don't need to do
+  /// this every time - only once.
+  G4bool   initialised;
 };
 
 inline  void BDSQuadStepper::SetBGrad(G4double aBGrad)
-{itsBGrad=aBGrad;
-}
+{itsBGrad=aBGrad;}
 
-inline G4double BDSQuadStepper::GetBGrad()
+inline G4double BDSQuadStepper::GetBGrad() const
 {return itsBGrad;}
 
 inline void BDSQuadStepper::StepperName()
