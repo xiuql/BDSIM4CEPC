980fb6af1b17f3cb17e2d657fb6ec994c9a3a825
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2015-08-10 16:59:54 +0100

    tidying

--- include/BDSSamplerHit.hh
+++ include/BDSSamplerHit.hh
@@ -1,12 +1,3 @@
-/* BDSIM code.    Version 1.0
-   Author: Grahame A. Blair, Royal Holloway, Univ. of London.
-   Last modified 5.11.2002
-   Copyright (c) 2002 by G.A.Blair.  ALL RIGHTS RESERVED. 
-
-   Modified 22.03.05 by J.C.Carter, Royal Holloway, Univ. of London.
-   Changed Samplers to account for plane and cylinder types (GABs code)
-*/
-
 #ifndef BDSSamplerHit_h
 #define BDSSamplerHit_h 1
 
@@ -20,18 +11,20 @@
 class BDSSamplerHit :public G4VHit
 {
 public:
-  BDSSamplerHit(G4String aName,
+  BDSSamplerHit(G4String    aName,
 		BDSParticle init,
 		BDSParticle prod,
 		BDSParticle last_scat,
 		BDSParticle local,
 		BDSParticle global,
-		G4double s,
-		G4double weight,
-		G4int PDGtype, G4int nEvent,
-		G4int ParentID, G4int TrackID,
-		G4int TurnsTaken,
-		G4String sampType);
+		G4double    s,
+		G4double    weight,
+		G4int       PDGtype,
+		G4int       nEvent,
+		G4int       ParentID,
+		G4int       TrackID,
+		G4int       TurnsTaken,
+		G4String    sampType);
 
   ~BDSSamplerHit();
   
