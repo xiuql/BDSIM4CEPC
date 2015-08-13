650896ba746abe5520142bef766557ca0b34d56a
Jochem Snuverink <jochem.snuverink@rhul.ac.uk>
2015-08-13 10:57:16 +0100

    clean main: move visualisation code into new BDSVisManager class

--- bdsim.cc
+++ bdsim.cc
@@ -7,22 +7,6 @@
 #include "BDSExecOptions.hh"     // executable command line options 
 #include "BDSGlobalConstants.hh" //  global parameters
 
-#include "G4UIterminal.hh"
-#ifdef G4UI_USE_TCSH
-#include "G4UItcsh.hh"
-#endif
-
-#ifdef G4VIS_USE
-#include "G4VisExecutive.hh"
-#endif
-
-#ifdef G4UI_USE
-#ifdef G4VIS_USE
-#include "G4UImanager.hh"        // G4 session managers
-#endif
-#include "G4UIExecutive.hh"
-#endif
-
 #include <cstdlib>      // standard headers 
 #include <cstdio>
 #include <signal.h>
@@ -32,7 +16,6 @@
 #include "G4TrackingManager.hh"
 #include "G4SteppingManager.hh"
 #include "G4GeometryTolerance.hh"
-#include "G4TrajectoryDrawByCharge.hh"
 
 #include "BDSAcceleratorModel.hh"
 #include "BDSBunch.hh"
@@ -53,6 +36,7 @@
 #include "BDSRandom.hh" // for random number generator from CLHEP
 #include "BDSRunManager.hh"
 #include "BDSUtilities.hh"
+#include "BDSVisManager.hh"
 
 #include "parser/gmad.h"  // GMAD parser
 #include "parser/options.h"
@@ -263,96 +247,8 @@ int main(int argc,char** argv)
 
   if(!execOptions->GetBatch())   // Interactive mode
     {
-      G4UIsession* session=0;
-#ifdef G4UI_USE_TCSH
-      session = new G4UIterminal(new G4UItcsh);
-#else
-      session = new G4UIterminal();
-#endif
-
-#ifdef G4VIS_USE
-#ifdef BDSDEBUG 
-      G4cout<< __FUNCTION__ << "> Initializing Visualisation Manager"<<G4endl;
-#endif
-      // Initialize visualisation
-      G4VisManager* visManager = new G4VisExecutive;
-      // G4VisExecutive can take a verbosity argument - see /vis/verbose guidance.
-      // G4VisManager* visManager = new G4VisExecutive("Quiet");
-      visManager->Initialize();
-      
-      G4TrajectoryDrawByCharge* trajModel1 = new G4TrajectoryDrawByCharge("trajModel1");
-      visManager->RegisterModel(trajModel1);
-      visManager->SelectTrajectoryModel(trajModel1->Name());
-#endif
- 
-#ifdef G4UI_USE
-      G4UIExecutive* session2 = new G4UIExecutive(argc, argv);
-#ifdef G4VIS_USE
-      // get the pointer to the User Interface manager 
-      G4UImanager* UIManager = G4UImanager::GetUIpointer();
-
-      std::string bdsimPath = BDS::GetBDSIMExecPath();
-      // difference between local build and install build:
-      std::string visPath;
-      std::string localPath = bdsimPath + "vis/vis.mac";
-      std::string installPath = bdsimPath + "../share/BDSIM/vis/vis.mac";
-      
-      if (FILE *file = fopen(localPath.c_str(), "r")) {
-	fclose(file);
-	visPath = bdsimPath + "vis/";
-      } else if (FILE *file = fopen(installPath.c_str(), "r")) {
-	fclose(file);
-	visPath = bdsimPath + "../share/BDSIM/vis/";
-      } else {
-	G4cout << __FUNCTION__ << "> ERROR: default visualisation file could not be found!" << G4endl;
-      }
-
-      // check if visualisation file is present and readable
-      std::string visMacroName = execOptions->GetVisMacroFilename();
-      bool useDefault = false;
-      // if not set use default visualisation file
-      if (visMacroName.empty()) useDefault = true;
-      G4String visMacroFilename = BDS::GetFullPath(visMacroName);
-      if (!useDefault) {
-	FILE* file = nullptr;
-	// first relative to main path:
-	file = fopen(visMacroFilename.c_str(), "r");
-	if (file) {
-	  fclose(file);
-	} else {
-	  // if not present use a default one (OGLSQt or DAWNFILE)
-	  G4cout << __FUNCTION__ << "> WARNING: visualisation file " << visMacroFilename <<  " file not present, using default!" << G4endl;
-	  useDefault = true;
-	}
-      }
-      if (useDefault) {
-#ifdef G4VIS_USE_OPENGLQT
-	visMacroFilename = visPath + "vis.mac";
-#else
-	visMacroFilename = visPath + "dawnfile.mac";
-#endif
-      }
-      // execute visualisation file
-      UIManager->ApplyCommand("/control/execute " + visMacroFilename);
-
-      // add default gui
-      if (session2->IsGUI()) {
-	// Add icons
-	std::string iconMacroFilename = visPath + "icons.mac";
-	UIManager->ApplyCommand("/control/execute " + iconMacroFilename);
-	// add menus
-	std::string guiMacroFilename  = visPath + "gui.mac";
-	UIManager->ApplyCommand("/control/execute " + guiMacroFilename);
-	// add run icon:
-	std::string runButtonFilename = visPath + "run.png";
-	UIManager->ApplyCommand("/gui/addIcon \"Run beam on\" user_icon \"/run/beamOn 1\" " + runButtonFilename);
-      }
-#endif
-      session2->SessionStart();
-      delete session2;
-#endif
-      delete session;
-
+      BDSVisManager visManager;
+      visManager.StartSession(argc,argv);
     }
   else           // Batch mode
     { 
