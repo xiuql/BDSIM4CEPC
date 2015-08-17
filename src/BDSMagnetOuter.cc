3dfd40d240abfa3813ce31b129467695ef6ede06
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2015-08-17 09:56:01 +0100

    introduce MagnetOuter class which inherits BDSGeometryComponent with the addition of another BDSGeometryComponent that represents the suggested container for the whole magnet - only the outer factory knows the real shape of the magnet outer part and therefore is the only part that can make a tight fitting container. Use this class to pass that information around

---
+++ src/BDSMagnetOuter.cc
@@ -0,0 +1,40 @@
+#include "BDSGeometryComponent.hh"
+#include "BDSMagnetOuter.hh"
+
+#include <utility>
+
+class G4VSolid;
+class G4LogicalVolume;
+
+BDSMagnetOuter::BDSMagnetOuter(G4VSolid*                    containerSolid,
+			       G4LogicalVolume*             containerLV,
+			       std::pair<G4double,G4double> extentX,
+			       std::pair<G4double,G4double> extentY,
+			       std::pair<G4double,G4double> extentZ,
+			       BDSGeometryComponent*        magnetContainerIn,
+			       G4ThreeVector                placementOffset):
+  BDSGeometryComponent(containerSolid, containerLV, extentX, extentY, extentZ,
+		       placementOffset),
+  magnetContainer(magnetContainerIn)
+{;}
+
+BDSMagnetOuter::BDSMagnetOuter(BDSGeometryComponent* componentIn,
+			       BDSGeometryComponent* magnetContainerIn):
+  BDSGeometryComponent(*componentIn),
+  magnetContainer(magnetContainerIn)
+{;}
+
+void BDSMagnetOuter::ClearMagnetContainer()
+{
+  if (magnetContainer)
+    {
+      delete magnetContainer;
+      magnetContainer = nullptr;
+    }
+}
+
+BDSMagnetOuter::~BDSMagnetOuter()
+{
+  ClearMagnetContainer();
+  BDSGeometryComponent::~BDSGeometryComponent();
+}
