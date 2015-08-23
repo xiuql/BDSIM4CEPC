3f290c3de297d7f356d1b3b971defc16575214c0
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2015-08-23 10:58:19 +0100

    Try to fix cycle numbers

--- src/BDSOutputROOT.cc
+++ src/BDSOutputROOT.cc
@@ -25,7 +25,7 @@ BDSOutputROOT::BDSOutputROOT()
 BDSOutputROOT::~BDSOutputROOT()
 {
   if (theRootOutputFile && theRootOutputFile->IsOpen())
-    {theRootOutputFile->Write();}
+    {theRootOutputFile->Write(0,TObject::kOverwrite);}
 }
 
 void BDSOutputROOT::BuildSamplerTree(G4String name)
@@ -604,7 +604,7 @@ void BDSOutputROOT::Write()
       G4cout << __METHOD_NAME__ << " - ROOT file found and open, writing." << G4endl;
 #endif
       //Dump all other quantities to file...
-      theRootOutputFile->Write();
+      theRootOutputFile->Write(0,TObject::kOverwrite);
       theRootOutputFile->Close();
       delete theRootOutputFile;
       theRootOutputFile=nullptr;
