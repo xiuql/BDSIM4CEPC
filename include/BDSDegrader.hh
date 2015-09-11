57c3cc5e5aeb541cdf8b46a08680a54a49c7dd9f
William Shields <william.shields.2010@live.rhul.ac.uk>
2015-09-11 14:48:36 +0100

    Start of degrader element.

---
+++ include/BDSDegrader.hh
@@ -0,0 +1,47 @@
+#ifndef BDSDEGRADER_H
+#define BDSDEGRADER_H
+
+#include "globals.hh"
+
+#include "BDSAcceleratorComponent.hh"
+
+class G4VSolid;
+
+/**
+ * @brief A singleton class that holds all required sensitive
+ * detector class instances.  Each sensitive detector class
+ * need only be instantiated once and attached to the relevant
+ * volume. More factorised geometry construction will mean
+ * SDs will be assigned in construction and not in detector
+ * construction afterwards as a post processing step. This 
+ * instantiates all necessary SDs and holds them.
+ * 
+ * @author Laurie Nevay <Laurie.Nevay@rhul.ac.uk>
+ */
+
+class BDSDegrader: public BDSAcceleratorComponent
+{
+public:
+    BDSDegrader(G4String name, 
+             G4double   length,
+             G4double   outerDiameter,
+             G4String   degraderMaterial,
+             G4int      numberWedges,
+             G4double   wedgeBasewidth,
+             G4double   wedgeHeight,
+             G4double   degraderHeight);
+    ~BDSDegrader();
+    
+
+protected:
+    void Build();
+    
+    
+    void BuildContainerLogicalVolume();
+    
+    G4VSolid* fullwedge;
+    G4VSolid* halfWedge;
+    
+};
+
+#endif
---
+++ src/BDSDegrader.cc
@@ -0,0 +1,38 @@
+#include "BDSAcceleratorComponent.hh"
+#include "BDSGlobalConstants.hh" 
+#include "BDSDegrader.hh"
+
+#include "G4Box.hh"
+#include "G4VisAttributes.hh"
+#include "G4LogicalVolume.hh"
+#include "G4PVPlacement.hh"               
+#include "G4SubtractionSolid.hh"
+#include "G4ExtrudedSolid.hh"
+
+
+
+#include "globals.hh" // geant4 globals / types
+
+BDSDegrader::BDSDegrader (G4String   name, 
+                    G4double   length,
+                    G4double   outerDiameter,
+                    G4String   degraderMaterial,
+                    G4int      numberWedges,
+                    G4double   wedgeBasewidth,
+                    G4double   wedgeHeight,
+                    G4double   degraderHeight):
+BDSAcceleratorComponent(name, length, 0, "degrader")
+{;}
+
+BDSDegrader::~BDSDegrader()
+{;}
+
+void BDSDegrader::BuildContainerLogicalVolume()
+{
+
+}
+
+void BDSDegrader::Build()
+{  
+
+}
