56c73ee7e5e5e57382ee08b440cf9a582940f776
William Shields <william.shields.2010@live.rhul.ac.uk>
2015-09-11 16:11:28 +0100

    Minor parameter change.

--- include/BDSDegrader.hh
+++ include/BDSDegrader.hh
@@ -23,13 +23,14 @@ class BDSDegrader: public BDSAcceleratorComponent
 {
 public:
     BDSDegrader(G4String name, 
-		G4double   length,
-		G4double   outerDiameter,
-		G4String   degraderMaterial,
-		G4int      numberWedges     = 3,
-		G4double   wedgeBasewidth   = 10,
-		G4double   wedgeHeight      = 40,
-		G4double   degraderHeight   = 100);
+                G4double   length,
+                G4double   outerDiameter,
+                G4String   degraderMaterial,
+                G4int      numberWedges     = 3,
+                G4double   wedgeBasewidth   = 10,
+                G4double   wedgeHeight      = 40,
+                G4double   degraderHeight   = 100,
+                G4double   materialThickness= 200);
     ~BDSDegrader();
     
 
