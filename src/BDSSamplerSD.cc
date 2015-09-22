be40d2dad39aaa832d9d7f595eaa95dd78d0f26c
Jochem Snuverink <jochem.snuverink@rhul.ac.uk>
2015-09-22 16:05:00 +0100

    update BDSSamplerSD for volume naming

--- src/BDSSamplerSD.cc
+++ src/BDSSamplerSD.cc
@@ -100,9 +100,12 @@ G4bool BDSSamplerSD::ProcessHits(G4Step*aStep,G4TouchableHistory*)
   
   nEvent+=BDSGlobalConstants::Instance()->GetEventNumberOffset();
   
-  G4int nSampler    = theTrack->GetVolume()->GetCopyNo()+1;
-  G4String SampName = theTrack->GetVolume()->GetName()+"_"+BDS::StringFromInt(nSampler);
-  SampName = SampName.substr(0,SampName.find("_pv_1"));
+  G4int nSampler    = theTrack->GetVolume()->GetCopyNo();
+  G4String SampName = theTrack->GetVolume()->GetName();
+  // for now remove copy number so sampler name has no knowledge of the copy number
+  // only one sampler per copied element (will be fixed in the future)
+  std::string removeEnd = "_" + std::to_string(nSampler) + "_pv";
+  SampName = SampName.substr(0,SampName.find(removeEnd));
   
   G4int    PDGtype = theTrack->GetDefinition()->GetPDGEncoding();
   G4String pName   = theTrack->GetDefinition()->GetParticleName();
