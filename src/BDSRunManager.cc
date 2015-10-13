b8169fe0d97d5c84f2190da3acb21e75525f7066
Jochem Snuverink <jochem.snuverink@rhul.ac.uk>
2015-10-13 13:04:38 +0200

    improve cout

--- src/BDSRunManager.cc
+++ src/BDSRunManager.cc
@@ -14,10 +14,10 @@ void BDSRunManager::DoEventLoop(G4int n_event,const char* macroFile,G4int n_sele
   // save event loop state
   if(verboseLevel>0){
     // Print seed to try and recreate an event in a run 
-    G4cout << __METHOD_NAME__ << "> Random number generator's seed=" 
+    G4cout << __METHOD_NAME__ << "Random number generator's seed=" 
 	   << CLHEP::HepRandom::getTheSeed() << G4endl;
     // Print generator full state to output 
-    G4cout << __METHOD_NAME__ << "> Random number generator's state: " << G4endl;
+    G4cout << __METHOD_NAME__ << "Random number generator's state: " << G4endl;
     CLHEP::HepRandom::saveFullState(G4cout);
   }
 
@@ -28,12 +28,12 @@ void BDSRunManager::ProcessOneEvent(G4int i_event)
 {
   // additional output
   if(verboseLevel>3){
-    G4cout << __METHOD_NAME__ << " Event="<<i_event<<G4endl;
+    G4cout << __METHOD_NAME__ << "Event="<<i_event<<G4endl;
     // Print seed to try and recreate an event in a run
-    G4cout << __METHOD_NAME__ << "> Random number generator's seed=" 
+    G4cout << __METHOD_NAME__ << "Random number generator's seed=" 
 	   << CLHEP::HepRandom::getTheSeed() << G4endl;
     // Print generator full state to output 
-    G4cout << __METHOD_NAME__ << " Random number generator's state: " << G4endl;
+    G4cout << __METHOD_NAME__ << "Random number generator's state: " << G4endl;
     CLHEP::HepRandom::saveFullState(G4cout);
   }
 
