63fc90cf5b1a546e23ec4d34b8c8df6c86496ebb
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2015-10-25 22:43:44 +0000

    fix issue 78.  Rework sector bend dipole field to get local coordinates.

--- include/BDSSbendMagField.hh
+++ include/BDSSbendMagField.hh
@@ -1,29 +1,49 @@
-#ifndef BDS_SBEND_FIELD_HH
-#define BDS_SBEND_FIELD_HH
+#ifndef BDSSBENDFIELD_H
+#define BDSSBENDFIELD_H
 
 #include "globals.hh"
-#include "G4ThreeVector.hh"
-#include "G4UniformMagField.hh"
-#include "G4Mag_EqRhs.hh"
+#include "G4MagneticField.hh"
 
-class BDSSbendMagField: public G4UniformMagField 
+#include "BDSAuxiliaryNavigator.hh"
+
+class BDSSbendMagField: public G4MagneticField, public BDSAuxiliaryNavigator
 {
-public:  // with description
+public:
+  /// A constant uniform field with value equal to 'field'. If angle is
+  /// finite, the local radius of curvature is calculated and used by 
+  BDSSbendMagField(G4ThreeVector  fieldIn,
+		   const G4double aLength,
+		   const G4double aAngle  = 0);
   
-  BDSSbendMagField(const G4ThreeVector& FieldVector,const G4double aLength,
-		    const G4double aAngle);
-  // A field with value equal to FieldVector.
 		       
-  ~BDSSbendMagField() ;
+  virtual ~BDSSbendMagField();
 
-  G4double GetLocalRadius();
+  /// The method Geant4 will use to access the field value
+  virtual void  GetFieldValue(const G4double point[4],
+			      G4double* bField) const;
+
+  /// Access the local radius of curvature for the nominal momentum in this field.
+  G4double GetLocalRadius() const;
 
 private:
-  G4double itsLocalRadius ;
+  /// Rotate the field vector to global coordinates using an example point. This gives
+  /// a G4AffineTransform and the rotation (only) from this is used to rotate the field
+  /// vector to global coordinates.
+  void TransformFieldToGlobal(const G4double point[4]);
+  
+  /// The vector of the field. This constructed in local coordinates and the first time
+  /// the field is queried, the auxiliary navigator (through inheritance) is used to
+  /// transform the vector to global coordinates.
+  G4ThreeVector field;
 
+  /// A record of whether the field vector has been transformed to global coordinates.
+  G4bool fieldTransformed;
+  
+  /// Radius of curvature.
+  G4double localRadius;
 };
 
-inline G4double BDSSbendMagField::GetLocalRadius()
-{return itsLocalRadius;}
+inline G4double BDSSbendMagField::GetLocalRadius() const
+{return localRadius;}
 
 #endif
