9e4bd4b4a0cf49aebd24c356381d0e08c339b122
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2015-11-03 14:50:04 +0000

    Introduce another beam pipe factory class - BDSBeamPipeFactoryPoints - that is pure virtual and is used for any beam pipe factory that would use points to define an extruded solid.  No support for scaling or extrusion just now.

---
+++ include/BDSBeamPipeFactoryPoints.hh
@@ -0,0 +1,149 @@
+#ifndef BDSBEAMPIPEFACTORYPOINTS_H
+#define BDSBEAMPIPEFACTORYPOINTS_H
+
+#include "BDSBeamPipeFactoryBase.hh"
+#include "BDSBeamPipe.hh"
+
+#include "globals.hh"
+#include "G4TwoVector.hh"
+
+#include <vector>
+
+/**
+ * @brief Factory for racetrack aperture model beampipes.
+ * 
+ * This is a pure virtual class that provides common functionality
+ * for aperture models derived from a series of points transversely.
+ * The derived class need therefore only generate the vector of points
+ * that will make the edges of the vacuum and inner & outer beam pipe 
+ * surfaces. The solids are of type G4ExtrudedSolid, or in the case of angled 
+ * faces, a G4ExtrudedSolid intersected with a G4CutTubs (cylinder with 
+ * angled faces).
+ * 
+ * @author Laurie Nevay <laurie.nevay@rhul.ac.uk>
+ */
+
+class G4Material;
+
+class BDSBeamPipeFactoryPoints: public BDSBeamPipeFactoryBase
+{
+public:
+  BDSBeamPipeFactoryPoints();
+  virtual ~BDSBeamPipeFactoryPoints();
+
+  /// Required overloaded method from BDSBeamPipeFactoryBase to build straight piece of beam pipe
+  virtual BDSBeamPipe* CreateBeamPipe(G4String    nameIn,
+				      G4double    lengthIn,
+				      G4double    aper1In,
+				      G4double    aper2In,
+				      G4double    aper3In,
+				      G4double    aper4In,
+				      G4Material* vacuumMaterialIn,
+				      G4double    beamPipeThicknessIn,
+				      G4Material* beamPipeMaterialIn);
+
+  /// Required overloaded method from BDSBeamPipeFactoryBase to build angled piece of beam pipe
+  virtual BDSBeamPipe* CreateBeamPipeAngledInOut(G4String    nameIn,
+						 G4double    lengthIn,
+						 G4double    angleInIn,
+						 G4double    angleOutIn,
+						 G4double    aper1In,
+						 G4double    aper2In,
+						 G4double    aper3In,
+						 G4double    aper4In,
+						 G4Material* vacuumMaterialIn,
+						 G4double    beamPipeThicknessIn,
+						 G4Material* beamPipeMaterialIn);
+
+  /// Function to generate transverse points.  Should set member variables vacuumEdge,
+  /// beamPipeInnerEdge, beamPipeOuterEdge, containerEdge and containerSubtractionEdge.
+  virtual void GeneratePoints(G4double aper1,
+			      G4double aper2,
+			      G4double aper3,
+			      G4double aper4,
+			      G4double beamPipeThickness,
+			      G4int    pointsPerTwoPi = 40) = 0;
+
+  /// Function derived class must provide to calculated the intersection radius, which
+  /// is the radius of the intersection
+  /// cylinder to create the aperture faces. This should be big enough to contain the
+  /// cross-section of the beam pipe but not overly large, so as to avoid intersecting plane
+  /// errors (and therefore exits) from large angles on short elements.
+  virtual G4double CalculateIntersectionRadius(G4double aper1,
+					       G4double aper2,
+					       G4double aper3,
+					       G4double aper4,
+					       G4double beamPipeThickness) = 0;
+
+  /// Test the input parameter validity.  Must be provided by derived class.  Returns void
+  /// but all parameters passed by reference.  Overloaded function should set to minimum
+  /// vialbe value or exit on error explaining why.
+  virtual void TestInputParameters(G4Material*& vacuumMaterial,
+				   G4double&    beamPipeThickness,
+				   G4Material*& beamPipeMaterial,
+				   G4double&    aper1,
+				   G4double&    aper2,
+				   G4double&    aper3,
+				   G4double&    aper4) = 0;
+
+  /// Clear member vectors and run base class clean up to clear pointers between runs
+  virtual void CleanUp();
+
+protected:
+  /// Vector of x,y coordinates for vacuum extruded solid edge.
+  std::vector<G4TwoVector> vacuumEdge;
+
+  /// Vector of x,y coordinates for beam pipe inner edge.
+  std::vector<G4TwoVector> beamPipeInnerEdge;
+
+  /// Vector of x,y coordinates for beam pipe outer edge.
+  std::vector<G4TwoVector> beamPipeOuterEdge;
+
+  /// Vector of x,y coordinates for the container volume.
+  std::vector<G4TwoVector> containerEdge;
+
+  /// Vector of x,y coordinates for the container subtraction volume.
+  std::vector<G4TwoVector> containerSubtractionEdge;
+
+
+private:
+  /// Radius of intersection cylinder for angled faced beam pipe pieces. Should
+  /// be determined and set in GeneratePoints() by derived class. Should be big
+  /// enough to enclose the beam pipe section transversely but not too large that
+  /// overlapping z planes errors might occur with short pieces of beam pipe with
+  /// large angled faces.
+  G4double intersectionRadius;
+
+  ///@{ Extents prepared by GeneratePoints function as only it knows the extents
+  G4double extentX;
+  G4double extentY;
+  ///@}
+  
+  /// Solid for innter surface of beam pipe.
+  G4VSolid* beamPipeInnerSolid;
+  
+  /// Solid for outer surface of beam pipe.
+  G4VSolid* beamPipeOuterSolid;
+
+  /// Create the vacuum, inner & outer, container and container subtraction solids.
+  /// Optional flag buildLongForIntersection builds the solids longer by 1.5x length
+  /// in preparation for them to be intersected for angled faces, saving code duplication.
+  void CreateSolids(G4String name,
+		    G4double length,
+		    G4bool   buildLongForIntersection = false);
+
+  /// Create angled solids.  Uses CreateSolids() method.
+  void CreateSolidsAngled(G4String      name,
+			  G4double      length,
+			  G4ThreeVector inputFace,
+			  G4ThreeVector outputFace);
+
+  /// Overloads BDSBeamPipeFactorBase method to do common construction tasks and construct
+  /// BDSBeamPipe instance to return.
+  virtual BDSBeamPipe* CommonFinalConstruction(G4String    nameIn,
+					       G4Material* vacuumMaterialIn,
+					       G4Material* beamPipeMaterialIn,
+					       G4double    lengthIn);
+};
+  
+#endif
---
+++ src/BDSBeamPipeFactoryPoints.cc
@@ -0,0 +1,249 @@
+#include "BDSBeamPipeFactoryPoints.hh"
+#include "BDSBeamPipeFactoryElliptical.hh"
+#include "BDSBeamPipe.hh"
+
+#include "BDSDebug.hh"
+#include "BDSUtilities.hh"
+
+#include "globals.hh"                 // geant4 globals / types
+#include "G4CutTubs.hh"
+#include "G4ExtrudedSolid.hh"
+#include "G4IntersectionSolid.hh"
+#include "G4Material.hh"
+#include "G4SubtractionSolid.hh"
+#include "G4ThreeVector.hh"
+#include "G4TwoVector.hh"
+#include "G4VSolid.hh"
+
+#include <cmath>                           // sin, cos, fabs
+#include <utility>                         // for std::pair
+
+BDSBeamPipeFactoryPoints::BDSBeamPipeFactoryPoints()
+{
+  CleanUp();
+}
+
+BDSBeamPipeFactoryPoints::~BDSBeamPipeFactoryPoints()
+{;}
+
+void BDSBeamPipeFactoryPoints::CleanUp()
+{
+  intersectionRadius = 0;
+  extentX            = 0;
+  extentY            = 0;
+  
+  vacuumEdge.clear();
+  beamPipeInnerEdge.clear();
+  beamPipeOuterEdge.clear();
+  containerEdge.clear();
+  containerSubtractionEdge.clear();
+
+  beamPipeInnerSolid = nullptr;
+  beamPipeOuterSolid = nullptr;
+
+  BDSBeamPipeFactoryBase::CleanUp();
+}
+
+void BDSBeamPipeFactoryPoints::CreateSolids(G4String name,
+					    G4double length,
+					    G4bool   buildLongForIntersection)
+{
+#ifdef BDSDEBUG
+  G4cout << __METHOD_NAME__ << G4endl;
+#endif
+  /// Make the solids all longer for intersection
+  G4double zHalfLength = 0.5*length;
+  if (buildLongForIntersection)
+    {zHalfLength *= 1.5;}
+  
+  G4TwoVector zOffsets(0,0); // the transverse offset of each plane from 0,0
+  G4double zScale = 1; // the scale at each end of the points = 1
+  vacuumSolid = new G4ExtrudedSolid(name + "_vacuum_solid", // name
+				    vacuumEdge,             // vector of TwoVector points
+				    zHalfLength,            // half length for +- planes
+				    zOffsets, zScale,       // dx,dy offset for each face, scaling
+				    zOffsets, zScale);      // dx,dy offset for each face, scaling
+
+  beamPipeInnerSolid = new G4ExtrudedSolid(name + "_bp_inner_solid",
+					   beamPipeInnerEdge,
+					   zHalfLength * 1.5,
+					   zOffsets, zScale,
+					   zOffsets, zScale);
+  
+  beamPipeOuterSolid = new G4ExtrudedSolid(name + "_bp_outer_solid",
+					   beamPipeOuterEdge,
+					   zHalfLength,
+					   zOffsets, zScale,
+					   zOffsets, zScale);
+
+  allSolids.push_back(beamPipeInnerSolid);
+  allSolids.push_back(beamPipeOuterSolid);
+  
+  beamPipeSolid = new G4SubtractionSolid(name + "_pipe_solid", // name
+					 beamPipeOuterSolid,   // this
+					 beamPipeInnerSolid);  // minus this
+
+  containerSolid = new G4ExtrudedSolid(name + "_container_solid",
+				       containerEdge,
+				       zHalfLength,
+				       zOffsets, zScale,
+				       zOffsets, zScale);
+
+  containerSubtractionSolid = new G4ExtrudedSolid(name + "_container_subtraction_solid",
+						  containerSubtractionEdge,
+						  zHalfLength*1.5,
+						  zOffsets, zScale,
+						  zOffsets, zScale);
+}
+
+void BDSBeamPipeFactoryPoints::CreateSolidsAngled(G4String      name,
+						  G4double      length,
+						  G4ThreeVector inputFace,
+						  G4ThreeVector outputFace)
+{
+#ifdef BDSDEBUG
+  G4cout << __METHOD_NAME__ << G4endl;
+#endif
+  
+  // create straight solids that are a bit long
+  CreateSolids(name + "_straight", length, true);
+
+  // now intersect them with one G4CutTubs to get the angled faces
+  G4double zHalfLength          = length*0.5 - lengthSafety;
+  G4double zHalfLengthContainer = length*0.5;
+  
+  G4VSolid* faceSolid = new G4CutTubs(name + "_face_solid", // name
+				      0,                    // inner radius
+				      intersectionRadius,   // outer radius
+				      zHalfLength,          // z half length
+				      0,                    // start angle
+				      CLHEP::twopi,         // sweep angle
+				      inputFace,            // input face normal
+				      outputFace);          // output face normal
+
+  G4VSolid* faceSolidContainer = new G4CutTubs(name + "_cont_face_solid", // name
+					       0,                         // inner radius
+					       intersectionRadius,        // outer radius
+					       zHalfLengthContainer,      // z half length
+					       0,                         // start angle
+					       CLHEP::twopi,              // sweep angle
+					       inputFace,                 // input face normal
+					       outputFace);               // output face normal
+
+  allSolids.push_back(faceSolid);
+  allSolids.push_back(faceSolidContainer);
+
+  // copy pointers to do intersection with then reassign member pointer
+  // to point to new solid
+  G4VSolid* vacuumTemp = vacuumSolid;
+  vacuumSolid = new G4IntersectionSolid(name + "_vacuum_solid",
+					vacuumTemp,
+					faceSolid);
+
+  G4VSolid* beamPipeTemp = beamPipeSolid;
+  beamPipeSolid = new G4IntersectionSolid(name + "_pipe_solid",
+					  beamPipeTemp,
+					  faceSolid);
+
+  G4VSolid* containerTemp = containerSolid;
+  containerSolid = new G4IntersectionSolid(name + "_container_solid",
+					   containerTemp,
+					   faceSolidContainer);
+
+  // container subtraction solid can just be long with flat edges as
+  // only used transversely
+}
+
+BDSBeamPipe* BDSBeamPipeFactoryPoints::CreateBeamPipe(G4String    nameIn,              // name
+						      G4double    lengthIn,            // length [mm]
+						      G4double    aper1In,             // aperture parameter 1
+						      G4double    aper2In,             // aperture parameter 2
+						      G4double    aper3In,             // aperture parameter 3
+						      G4double    aper4In,             // aperture parameter 4
+						      G4Material* vacuumMaterialIn,    // vacuum material
+						      G4double    beamPipeThicknessIn, // beampipe thickness [mm]
+						      G4Material* beamPipeMaterialIn)  // beampipe material
+{
+#ifdef BDSDEBUG
+  G4cout << __METHOD_NAME__ << G4endl;
+#endif
+  // clean up after last usage
+  CleanUp();
+
+  // test input parameters
+  TestInputParameters(vacuumMaterialIn, beamPipeThicknessIn, beamPipeMaterialIn, aper1In, aper2In, aper3In, aper4In);
+
+  // generate extruded solid edges - provided by derived class
+  GeneratePoints(aper1In, aper2In, aper3In, aper4In, beamPipeThicknessIn);
+
+  // calculate and set the intersection solid radius
+  intersectionRadius = CalculateIntersectionRadius(aper1In, aper2In, aper3In, aper4In, beamPipeThicknessIn);
+
+  // create solids based on the member vectors of points
+  CreateSolids(nameIn, lengthIn);
+					
+  return CommonFinalConstruction(nameIn, vacuumMaterialIn, beamPipeMaterialIn, lengthIn);
+}
+
+BDSBeamPipe* BDSBeamPipeFactoryPoints::CreateBeamPipeAngledInOut(G4String    nameIn,              // name
+								 G4double    lengthIn,            // length [mm]
+								 G4double    angleInIn,           // the normal angle of the input face
+								 G4double    angleOutIn,          // the normal angle of the input face
+								 G4double    aper1In,             // aperture parameter 1
+								 G4double    aper2In,             // aperture parameter 2
+								 G4double    aper3In,             // aperture parameter 3
+								 G4double    aper4In,             // aperture parameter 4
+								 G4Material* vacuumMaterialIn,    // vacuum material
+								 G4double    beamPipeThicknessIn, // beampipe thickness [mm]
+								 G4Material* beamPipeMaterialIn)  // beampipe material
+{
+#ifdef BDSDEBUG
+  G4cout << __METHOD_NAME__ << G4endl;
+#endif
+  // clean up after last usage
+  CleanUp();
+
+  // test input parameters
+  TestInputParameters(vacuumMaterialIn, beamPipeThicknessIn, beamPipeMaterialIn, aper1In, aper2In, aper3In, aper4In);
+
+  // generate extruded solid edges - provided by derived class
+  GeneratePoints(aper1In, aper2In, aper3In, aper4In, beamPipeThicknessIn);
+  
+  std::pair<G4ThreeVector,G4ThreeVector> faces = CalculateFaces(angleInIn, angleOutIn);
+  G4ThreeVector inputface  = faces.first;
+  G4ThreeVector outputface = faces.second;
+
+  // calculate and set the intersection solid radius
+  intersectionRadius = CalculateIntersectionRadius(aper1In, aper2In, aper3In, aper4In, beamPipeThicknessIn);
+
+  // create solids based on the member vectors of points
+  CreateSolidsAngled(nameIn, lengthIn, inputface, outputface);
+  
+  return CommonFinalConstruction(nameIn, vacuumMaterialIn, beamPipeMaterialIn, lengthIn);
+}
+
+BDSBeamPipe* BDSBeamPipeFactoryPoints::CommonFinalConstruction(G4String    nameIn,
+							       G4Material* vacuumMaterialIn,
+							       G4Material* beamPipeMaterialIn,
+							       G4double    lengthIn)
+{
+#ifdef BDSDEBUG
+  G4cout << __METHOD_NAME__ << G4endl;
+#endif
+
+  BDSBeamPipeFactoryBase::CommonConstruction(nameIn,
+					     vacuumMaterialIn,
+					     beamPipeMaterialIn,
+					     lengthIn);
+
+  // record extents
+  std::pair<double,double> extX = std::make_pair(-extentX, extentX);
+  std::pair<double,double> extY = std::make_pair(-extentY, extentY);
+  std::pair<double,double> extZ = std::make_pair(-lengthIn*0.5, lengthIn*0.5);
+  // calculate radius if a tube were to be place around it
+  G4double containerRadius = std::max(extentX, extentY);
+  
+  BDSBeamPipe* aPipe = BuildBeamPipeAndRegisterVolumes(extX,extY,extZ,containerRadius);
+  
+  return aPipe;
+}
