bf9ee255740a928fab2c0bcee576745f84881b07
Jochem Snuverink <jochem.snuverink@rhul.ac.uk>
2015-11-19 20:50:10 +0000

    fix indentation

--- parser/gmad.h
+++ parser/gmad.h
@@ -4,7 +4,7 @@
  * bdsim v.0.3
  *
  * modification history:
-*/
+ */
 
 #ifndef _GMAD_H
 #define _GMAD_H
@@ -13,28 +13,28 @@
 #include <string>
 
 namespace GMAD {
-enum class ElementType;
+  enum class ElementType;
 
-// parse the input file and construct beamline_list and options 
-int gmad_parser(FILE *f);
+  // parse the input file and construct beamline_list and options 
+  int gmad_parser(FILE *f);
 
-int gmad_parser(std::string name);
+  int gmad_parser(std::string name);
 
-/** Python interface **/ 
-extern "C" {   
-  int    GmadParser_c(char *name);
+  /** Python interface **/ 
+  extern "C" {   
+    int    GmadParser_c(char *name);
 
-  /* Interface to extern beamline_list */
-  int          GetNElements();            // Length of list
-  int          GetType(int);              // Type of element 
-  const char*  GetName(int);              // Name of element
-  double       GetLength(int);            // Length of element
-  double       GetAngle(int);             // Angle of element
-  double*      GetKs(int);                // All magnetic ks
-  double       GetAper1(int);             // Aperture 1
-  double       GetAper2(int);             // Aperture 2
-  double       GetBeampipeThickness(int); // Beam Pipe Thickness
+    /* Interface to extern beamline_list */
+    int          GetNElements();            // Length of list
+    int          GetType(int);              // Type of element 
+    const char*  GetName(int);              // Name of element
+    double       GetLength(int);            // Length of element
+    double       GetAngle(int);             // Angle of element
+    double*      GetKs(int);                // All magnetic ks
+    double       GetAper1(int);             // Aperture 1
+    double       GetAper2(int);             // Aperture 2
+    double       GetBeampipeThickness(int); // Beam Pipe Thickness
 
-}
+  }
 }
 #endif
