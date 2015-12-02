a7b147b8d679d8c68e337be71c7f128eaa5deefa
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2015-12-02 22:12:15 +0000

    removed global coordinates

--- src/BDSOutputROOTEventSampler.cc
+++ src/BDSOutputROOTEventSampler.cc
@@ -1,3 +1,5 @@
+#include "CLHEP/Units/PhysicalConstants.h"
+
 #include "BDSOutputROOTEventSampler.hh"
 
 ClassImp(BDSOutputROOTEventSampler)
@@ -19,24 +21,25 @@ void BDSOutputROOTEventSampler::Fill(BDSSamplerHit *hit)
 {
   // get single values
   this->n++;
-  this->z = hit->GetZ();
-  this->Z = hit->GetGlobalZ();
-  this->S = hit->GetS();
+  this->z = hit->GetZ() / CLHEP::m;
+  this->S = hit->GetS() / CLHEP::m;
   
-  this->energy.push_back(hit->GetTotalEnergy());
-  this->x.push_back(hit->GetX());
-  this->y.push_back(hit->GetY());
+  this->energy.push_back(hit->GetTotalEnergy()  / CLHEP::GeV);
+  this->x.push_back(hit->GetX()                 / CLHEP::m);
+  this->y.push_back(hit->GetY()                 / CLHEP::m);
   
-  this->xp.push_back(hit->GetXPrime());
-  this->yp.push_back(hit->GetYPrime());
-  this->zp.push_back(hit->GetZPrime());
+  this->xp.push_back(hit->GetXPrime()           / CLHEP::radian);
+  this->yp.push_back(hit->GetYPrime()           / CLHEP::radian);
+  this->zp.push_back(hit->GetZPrime()           / CLHEP::radian);
   
-  this->X.push_back(hit->GetGlobalX());
-  this->Y.push_back(hit->GetGlobalY());
-  
-  this->Xp.push_back(hit->GetGlobalXPrime());
-  this->Yp.push_back(hit->GetGlobalYPrime());
-  this->Zp.push_back(hit->GetGlobalZPrime());
+#if 0
+  this->X.push_back(hit->GetGlobalX()           / CLHEP::m);
+  this->Y.push_back(hit->GetGlobalY()           / CLHEP::m);
+  this->Z = hit->GetGlobalZ()                   / CLHEP::m;  
+  this->Xp.push_back(hit->GetGlobalXPrime()     / CLHEP::radian);
+  this->Yp.push_back(hit->GetGlobalYPrime()     / CLHEP::radian);
+  this->Zp.push_back(hit->GetGlobalZPrime()     / CLHEP::radian);
+#endif
   
   this->weight.push_back(hit->GetWeight());
   this->partID.push_back(hit->GetPDGtype());
@@ -55,12 +58,14 @@ void BDSOutputROOTEventSampler::Clear()
   this->xp.clear();
   this->yp.clear();
   this->zp.clear();
+#if 0
   this->X.clear();
   this->Y.clear();
   this->Z = 0.0;      
   this->Xp.clear();
   this->Yp.clear();
   this->Zp.clear();
+#endif
   this->weight.clear();
   this->partID.clear();
   this->parentID.clear();
