69dc1ec7331824ba6e6a73229c48b21d6030ea05
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2016-01-25 15:57:31 +0000

    fixes to parallel world header

--- include/BDSParallelWorldSampler.hh
+++ include/BDSParallelWorldSampler.hh
@@ -1,18 +1,24 @@
 #ifndef BDSPARALLELWORLDSAMPER_H
 #define BDSPARALLELWORLDSAMPER_H
 
+#include "G4VPhysicalVolume.hh"
 #include "G4VUserParallelWorld.hh"
+
+#include <vector>
 
 class BDSParallelWorldSampler: public G4VUserParallelWorld
 {
   BDSParallelWorldSampler();
-  virtual ~BDSParallelWorldSampler(){;}
+  virtual ~BDSParallelWorldSampler();
 
   /// Construct the required parallel world geometry. This must
   /// overload the pure virutal method in G4VUserParallelWorld.
   virtual void Construct();
 
+private:
 
+  /// Cache of the placements to clean up at the end.
+  std::vector<G4VPhysicalVolume*> placements;
 };
 
 #endif
