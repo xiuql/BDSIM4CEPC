64789d50230ec8ff0ac7a20a9ab93aa9ff570193
William Shields <william.shields.2010@live.rhul.ac.uk>
2016-01-28 13:55:39 +0000

    start of refactoring rbend with angled polefaces.

--- include/BDSRBend.hh
+++ include/BDSRBend.hh
@@ -53,23 +53,6 @@ private:
   /// straight section in the rbend here.
   virtual void BuildBPFieldAndStepper();
 
-  /// Override method so we can build several bits of beam pipe
-  virtual void BuildBeampipe();
-
-  /// Override BDSMagnet::BuildOuter() so we can get a different length of central section
-  /// and magnet container.
-  virtual void BuildOuter();
-
-  /// Override BDSMagnet::BuildContainerLogicalVolume() so we can conditionally build the
-  /// correct container volume if no outer magnet geometry is built. In the case of an RBend
-  /// we can't simply use the central beam pipe segment as the container volume as it'll be too
-  /// small.
-  virtual void BuildContainerLogicalVolume();
-
-  /// Place the beam pipe and outer geometry in the overall container. If there's no outer
-  /// geometry, then we don't need to place either as the beam pipe becomes the container.
-  virtual void PlaceComponents();
-
   /// temporary function while old constructor still exists - used to avoid duplicating
   /// code in the mean time
   void CalculateLengths(G4double aLength);
