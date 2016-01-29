b8ef98203731ae12a303320e2a2c77ef8ddb5fcf
William Shields <william.shields.2010@live.rhul.ac.uk>
2016-01-29 14:22:46 +0000

    e1 and e2 pole face angles made available and passed into rbend for all magnet geometries.

--- src/BDSMagnetOuterFactoryLHC.cc
+++ src/BDSMagnetOuterFactoryLHC.cc
@@ -936,8 +936,8 @@ BDSMagnetOuter* BDSMagnetOuterFactoryLHC::CreateRectangularBend(G4String      na
 								G4double      containerDiameter,
 								G4double      containerLength,
 								G4double      angle,
-								G4double      /*e1*/,
-								G4double      /*e2*/,
+								G4double      e1,
+								G4double      e2,
 								G4Material*   outerMaterial)
 {
 #ifdef BDSDEBUG
@@ -950,8 +950,8 @@ BDSMagnetOuter* BDSMagnetOuterFactoryLHC::CreateRectangularBend(G4String      na
 									     containerDiameter,
 									     containerLength,
 									     angle,
-									     0,
-									     0,
+									     e1,
+									     e2,
 									     outerMaterial);
 }
 
--- src/BDSMagnetOuterFactoryPolesBase.cc
+++ src/BDSMagnetOuterFactoryPolesBase.cc
@@ -336,8 +336,8 @@ BDSMagnetOuter* BDSMagnetOuterFactoryPolesBase::CreateRectangularBend(G4String  
 								      G4double      containerDiameter,
 								      G4double      containerLength,
 								      G4double      angle,
-								      G4double      /*e1*/,
-								      G4double      /*e2*/,
+								      G4double      e1,
+								      G4double      e2,
 								      G4Material*   outerMaterial)
 {
 #ifdef BDSDEBUG
@@ -347,7 +347,7 @@ BDSMagnetOuter* BDSMagnetOuterFactoryPolesBase::CreateRectangularBend(G4String  
   
   return BDSMagnetOuterFactoryCylindrical::Instance()->CreateRectangularBend(name, length, beamPipe, outerDiameter,
 									     containerDiameter, containerLength,
-									     angle, 0, 0, outerMaterial);
+									     angle, e1, e2, outerMaterial);
 }
 
 BDSMagnetOuter* BDSMagnetOuterFactoryPolesBase::CreateQuadrupole(G4String      name,
