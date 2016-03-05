fd85993af7263b6797d16f917c6f16cdd7948741
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2016-03-05 19:55:51 +0000

    tidying and doxygen

--- include/BDSComptonEngine.hh
+++ include/BDSComptonEngine.hh
@@ -8,16 +8,22 @@
 #include "G4Step.hh"
 #include "CLHEP/Units/PhysicalConstants.h"
 
+/**
+ * @brief Engine to calcualte product of compton scattering process.
+ *
+ * @author Grahame Blair.
+ */
+
 class BDSComptonEngine  
 { 
-  public:
+public:
  
-     BDSComptonEngine();
-
-     BDSComptonEngine(G4LorentzVector InGam_FourVec, 
-                      G4LorentzVector InEl_FourVec );
- 
-    ~BDSComptonEngine();
+  BDSComptonEngine();
+  
+  BDSComptonEngine(G4LorentzVector InGam_FourVec, 
+		   G4LorentzVector InEl_FourVec );
+  
+  ~BDSComptonEngine();
 
   void PerformCompton();
   void PerformHighEnergyCompton();
@@ -27,21 +33,16 @@ class BDSComptonEngine
   void SetIncomingPhoton4Vec(G4LorentzVector inGam);
   void SetIncomingElectron4Vec(G4LorentzVector inEl);
 
-     G4LorentzVector GetScatteredElectron();
-     G4LorentzVector GetScatteredGamma();
-
-  protected:
-
-  private:
+  G4LorentzVector GetScatteredElectron();
+  G4LorentzVector GetScatteredGamma();
 
 private:
-    G4LorentzVector itsScatteredEl;
-    G4LorentzVector itsScatteredGam;
-    G4LorentzVector itsIncomingEl;
-    G4LorentzVector itsIncomingGam;
+  G4LorentzVector itsScatteredEl;
+  G4LorentzVector itsScatteredGam;
+  G4LorentzVector itsIncomingEl;
+  G4LorentzVector itsIncomingGam;
 
-   static const G4int ntryMax = 10000000;
-
+  static const G4int ntryMax = 10000000;
 };
 
 inline G4LorentzVector BDSComptonEngine::GetScatteredElectron()
@@ -49,7 +50,6 @@ inline G4LorentzVector BDSComptonEngine::GetScatteredElectron()
 
 inline G4LorentzVector BDSComptonEngine::GetScatteredGamma()
 {return itsScatteredGam;}
-
 
 inline void BDSComptonEngine::SetIncomingPhoton4Vec(G4LorentzVector inGam)
 {itsIncomingGam=inGam;
