e495f216fc4558bc41ea8d57b2ae48d9f8788321
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2016-03-05 23:43:02 +0000

    match style to rest of code base whilst debugging

--- include/BDSBOptrChangeCrossSection.hh
+++ include/BDSBOptrChangeCrossSection.hh
@@ -1,24 +1,38 @@
 #ifndef BDSBOPTRCHANGECROSSSECTION_H
 #define BDSBOPTRCHANGECROSSSECTION_H
 
+#include "globals.hh" // geant4 types / globals
 #include "G4VBiasingOperator.hh"
+
 class G4BOptnChangeCrossSection;
 class G4ParticleDefinition;
 
 #include <map>
 
-class BDSBOptrChangeCrossSection : public G4VBiasingOperator {
+/** 
+ * @brief Operator that changes cross section of a process for a particle.
+ *
+ * Based on the GB01 example from Geant4.
+ * 
+ * @author Stewart Boogert
+ */
+
+class BDSBOptrChangeCrossSection: public G4VBiasingOperator
+{
 public:
   BDSBOptrChangeCrossSection(G4String particleToBias, G4String name = "ChangeXS");
   virtual ~BDSBOptrChangeCrossSection();
+  
   virtual void StartRun();
   void SetBias(G4String processName, G4double dBias, G4int iPrimary);
 
 private: 
   virtual G4VBiasingOperation* ProposeOccurenceBiasingOperation(const G4Track* track,
 								const G4BiasingProcessInterface* callingProcess);
+
   virtual G4VBiasingOperation* ProposeFinalStateBiasingOperation(const G4Track*, const G4BiasingProcessInterface*) 
   {return 0;}
+
   virtual G4VBiasingOperation* ProposeNonPhysicsBiasingOperation(const G4Track*, const G4BiasingProcessInterface*)
   {return 0;}
 
@@ -36,7 +50,6 @@ private:
   G4bool                                                                 fSetup;
   G4String                                                               particleName;
   const G4ParticleDefinition*                                            fParticleToBias; 
-  G4bool debug;
 };
 
 #endif
