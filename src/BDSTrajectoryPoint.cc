a52b8c36595654c19f68d2b1ff73f4f2b9b0fabd
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2016-03-22 15:15:57 +0000

    tidy up trajectory point while debugging; style matching; initialise members; remove default constructor; remove unnecessary print out function; doxygen

--- src/BDSTrajectoryPoint.cc
+++ src/BDSTrajectoryPoint.cc
@@ -2,68 +2,66 @@
 #include <map>
 #include "G4Allocator.hh"
 #include "G4ProcessType.hh"
+#include "G4Step.hh"
+#include "G4Track.hh"
 #include "G4VProcess.hh"
 
 #include "globals.hh" // geant4 types / globals
+#include "G4ThreeVector.hh"
 
 #include <ostream>
 
 G4Allocator<BDSTrajectoryPoint> bdsTrajectoryPointAllocator;
 
-BDSTrajectoryPoint::BDSTrajectoryPoint()
+BDSTrajectoryPoint::BDSTrajectoryPoint():
+  G4TrajectoryPoint(G4ThreeVector())
 {
-  _currentProcess      = nullptr;
-  _isScatteringProcess = false;
-  _trackID             = -1;
+  currentProcess      = nullptr;
+  isScatteringProcess = false;
+  trackID             = -1;
+  vertexPosition      = G4ThreeVector();
 }
 
-BDSTrajectoryPoint::BDSTrajectoryPoint(const G4Track* aTrack):
-  G4TrajectoryPoint(aTrack->GetPosition())
+BDSTrajectoryPoint::BDSTrajectoryPoint(const G4Step* step):
+  G4TrajectoryPoint(step->GetPostStepPoint()->GetPosition())
 {
-  _currentProcess      = nullptr;
-  _isScatteringProcess = false;
-  _trackID             = -1;
-  _vertexPosition      = aTrack->GetVertexPosition();
-  _trackID             = aTrack->GetTrackID();
-  _currentProcess      = aTrack->GetStep()->GetPostStepPoint()->GetProcessDefinedStep();
+  G4Track* aTrack      = step->GetTrack();
+  currentProcess      = nullptr;
+  isScatteringProcess = false;
+  trackID             = -1;
+  vertexPosition      = aTrack->GetVertexPosition();
+  trackID             = aTrack->GetTrackID();
+  currentProcess      = aTrack->GetStep()->GetPostStepPoint()->GetProcessDefinedStep();
 
   G4ProcessType ptype = fNotDefined;
-  if(_currentProcess)
-    {ptype = _currentProcess->GetProcessType();}
+  if(currentProcess)
+    {ptype = currentProcess->GetProcessType();}
   
-  _isScatteringProcess = false;
+  isScatteringProcess = false;
   // If the process type is not undefined or transportation...
   if(!((ptype == fNotDefined) || (ptype == fTransportation))) 
     {
       // ...and the particle changed momentum during the step, then this is a "scattering"
       // (momentum-changing non-transportation) process.
-      const G4Step* step = aTrack->GetStep();
+      const G4Step* step    = aTrack->GetStep();
       G4ThreeVector pBefore = step->GetPreStepPoint()->GetMomentum();
       G4ThreeVector pAfter  = step->GetPostStepPoint()->GetMomentum();
       G4ThreeVector deltaP  = pAfter - pBefore;
       if (deltaP.x() != 0 || deltaP.y() != 0 || deltaP.z() != 0)
-	{_isScatteringProcess = true;}
+	{isScatteringProcess = true;}
     }
 }
 
-BDSTrajectoryPoint::~BDSTrajectoryPoint()
-{;}
-
 std::ostream& operator<< (std::ostream& out, BDSTrajectoryPoint const &p)
 {
-  if(p._currentProcess)
+  if(p.currentProcess)
     {
       out << "BDSTrajectoryPoint: ";
-      out << "current process = " << p._currentProcess->GetProcessName() << ", ";
+      out << "current process = " << p.currentProcess->GetProcessName() << ", that ";
       G4String result = "isn't";
-      if (p._isScatteringProcess)
+      if (p.isScatteringProcess)
 	{result = "is";}
       out << result << " a scattering process" << G4endl;
     }
   return out;
-}
-
-void BDSTrajectoryPoint::printData()
-{
-  G4cout << *(this) << G4endl;
 }
